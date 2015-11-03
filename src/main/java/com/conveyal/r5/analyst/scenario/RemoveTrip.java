5376e2e3468d122dfa1a612f4d22e6a62afa0bc1
Andrew Byrd <andrew@fastmail.net>
2015-11-03 14:54:18 +0100

    remove entire trip patterns when routes are filtered

--- src/main/java/com/conveyal/r5/analyst/scenario/RemoveTrip.java
+++ src/main/java/com/conveyal/r5/analyst/scenario/RemoveTrip.java
@@ -3,6 +3,8 @@ package com.conveyal.r5.analyst.scenario;
 import com.conveyal.r5.transit.TripPattern;
 import com.conveyal.r5.transit.TripSchedule;
 
+import java.util.Objects;
+
 /**
  * Remove trips from a scenario.
  * This could remove all trips on a route, since its superclass TimetableFilter provides both route and trip matching.
@@ -14,6 +16,23 @@ public class RemoveTrip extends TripScheduleModification {
     @Override
     public String getType() {
         return "remove-trip";
+    }
+
+    @Override
+    public TripPattern applyToTripPattern(TripPattern originalTripPattern) {
+        // Check first whether it's even possible that this Modification will apply to any trips on this pattern
+        if (!couldMatch(originalTripPattern)) {
+            return originalTripPattern;
+        }
+        // Stopgap efficient check for route filtration.
+        // All trips on a TripPattern are on the same route so we can remove them all at once.
+        if (routeId != null && (tripId == null || tripId.isEmpty())) {
+            if (routeId.contains(originalTripPattern.routeId)) {
+                return null;
+            }
+        }
+        // Actually apply the Modification to each TripSchedule within this TripPattern individually.
+        return super.applyToTripPattern(originalTripPattern);
     }
 
     @Override

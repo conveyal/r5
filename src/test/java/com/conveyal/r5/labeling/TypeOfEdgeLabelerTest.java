d9854dd38a62a9bdc63e8738f09fdbeed34f63a3
Marko Burjek <s0mebody.slo@gmail.com>
2015-11-27 14:15:24 +0100

    Chang typeLabeler test to parametrized test

--- src/test/java/com/conveyal/r5/labeling/TypeOfEdgeLabelerTest.java
+++ src/test/java/com/conveyal/r5/labeling/TypeOfEdgeLabelerTest.java
@@ -4,7 +4,11 @@ import com.conveyal.osmlib.Way;
 import com.conveyal.r5.streets.EdgeStore;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.EnumSet;
 
 import static org.junit.Assert.*;
@@ -12,10 +16,11 @@ import static org.junit.Assert.*;
 /**
  * Created by mabu on 27.11.2015.
  */
+@RunWith(Parameterized.class)
 public class TypeOfEdgeLabelerTest {
 
     public static final EnumSet<EdgeStore.EdgeFlag> PLATFORM = EnumSet
-        .of(EdgeStore.EdgeFlag.PLATFORM);
+        .of(EdgeStore.EdgeFlag.PLATFORM, EdgeStore.EdgeFlag.ALLOWS_PEDESTRIAN);
 
     static TypeOfEdgeLabeler typeOfEdgeLabeler;
     static TraversalPermissionLabeler traversalPermissionLabeler;
@@ -26,52 +31,54 @@ public class TypeOfEdgeLabelerTest {
         traversalPermissionLabeler = new USTraversalPermissionLabeler();
     }
 
+    //name of current test
+    private String name;
+    //Tags from which way is created
+    private String tags;
+    private EnumSet<EdgeStore.EdgeFlag> expectedForwardFlags;
+    private EnumSet<EdgeStore.EdgeFlag> expectedBackwardFlags;
+
+    public TypeOfEdgeLabelerTest(String name, String tags,
+        EnumSet<EdgeStore.EdgeFlag> expectedForwardFlags,
+        EnumSet<EdgeStore.EdgeFlag> expectedBackwardFlags) {
+        this.name = name;
+        this.tags = tags;
+        this.expectedForwardFlags = expectedForwardFlags;
+        this.expectedBackwardFlags = expectedBackwardFlags;
+    }
+
+    @Parameterized.Parameters(name="{0} with tags:{1}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+            {"sidewalk", "highway=cycleway;foot=designated;oneway=yes", EnumSet.of(EdgeStore.EdgeFlag.SIDEWALK,
+                EdgeStore.EdgeFlag.BIKE_PATH, EdgeStore.EdgeFlag.ALLOWS_BIKE, EdgeStore.EdgeFlag.ALLOWS_PEDESTRIAN),
+                EnumSet.of(EdgeStore.EdgeFlag.SIDEWALK,  EdgeStore.EdgeFlag.ALLOWS_PEDESTRIAN)
+            },
+            {"sidewalk", "highway=footway;footway=sidewalk", EnumSet.of(EdgeStore.EdgeFlag.SIDEWALK,
+                EdgeStore.EdgeFlag.ALLOWS_PEDESTRIAN), EnumSet.of(EdgeStore.EdgeFlag.SIDEWALK,
+                EdgeStore.EdgeFlag.ALLOWS_PEDESTRIAN)},
+            {"platform", "highway=platform", PLATFORM, PLATFORM},
+            {"platform", "public_transport=platform", PLATFORM, PLATFORM},
+            {"platform", "railway=platform", PLATFORM, PLATFORM},
+        } );
+
+
+    }
+
     @Test
-    public void testSidewalk() throws Exception {
-        Way osmWay = TraversalPermissionLabelerTest.makeOSMWayFromTags("highway=cycleway;foot=designated;oneway=yes");
-
-
-
+    public void testParam() throws Exception {
+        Way osmWay = TraversalPermissionLabelerTest.makeOSMWayFromTags(tags);
         EnumSet<EdgeStore.EdgeFlag> forwardFlags = traversalPermissionLabeler.getPermissions(osmWay, false);
         EnumSet<EdgeStore.EdgeFlag> backwardFlags = traversalPermissionLabeler.getPermissions(osmWay, true);
 
         typeOfEdgeLabeler.label(osmWay, forwardFlags, backwardFlags);
 
-        EnumSet<EdgeStore.EdgeFlag> expectedPermissionsForward = EnumSet.of(EdgeStore.EdgeFlag.SIDEWALK,
-            EdgeStore.EdgeFlag.BIKE_PATH, EdgeStore.EdgeFlag.ALLOWS_BIKE, EdgeStore.EdgeFlag.ALLOWS_PEDESTRIAN);
-        EnumSet<EdgeStore.EdgeFlag> expectedPermissionsBackward = EnumSet.of(EdgeStore.EdgeFlag.SIDEWALK,  EdgeStore.EdgeFlag.ALLOWS_PEDESTRIAN);
 
-        assertEquals(expectedPermissionsForward, forwardFlags);
-        assertEquals(expectedPermissionsBackward, backwardFlags);
+        assertEquals(expectedForwardFlags, forwardFlags);
+        assertEquals(expectedBackwardFlags, backwardFlags);
 
     }
 
-    @Test
-    public void testPlatform() throws Exception {
-        Way osmWay = TraversalPermissionLabelerTest.makeOSMWayFromTags("highway=platform");
 
-        EnumSet<EdgeStore.EdgeFlag> flag = EnumSet.noneOf(EdgeStore.EdgeFlag.class);
-
-        typeOfEdgeLabeler.label(osmWay, flag, flag);
-
-        assertEquals("highway=platform", PLATFORM, flag);
-
-
-        osmWay = TraversalPermissionLabelerTest.makeOSMWayFromTags("public_transport=platform");
-
-        flag = EnumSet.noneOf(EdgeStore.EdgeFlag.class);
-
-        typeOfEdgeLabeler.label(osmWay, flag, flag);
-
-        assertEquals("public_transport=platform", PLATFORM, flag);
-
-        osmWay = TraversalPermissionLabelerTest.makeOSMWayFromTags("railway=platform");
-
-        flag = EnumSet.noneOf(EdgeStore.EdgeFlag.class);
-
-        typeOfEdgeLabeler.label(osmWay, flag, flag);
-
-        assertEquals("railway=platform", PLATFORM, flag);
-    }
 
 }

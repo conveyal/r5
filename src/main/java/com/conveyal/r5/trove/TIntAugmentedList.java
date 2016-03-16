c87c6a6d08383e262b80039b079e7689fedf89f2
Andrew Byrd <andrew@fastmail.net>
2016-03-16 23:00:32 +0100

    add size and set methods to extended TIntList type

--- src/main/java/com/conveyal/r5/trove/TIntAugmentedList.java
+++ src/main/java/com/conveyal/r5/trove/TIntAugmentedList.java
@@ -14,8 +14,8 @@ import java.util.Random;
  * This TIntArrayList extension wraps another TIntList, and only allows extending it and retrieving values.
  * It assumes the wrapped TIntList is immutable and treats it as such.
  * NOTE that most methods will not behave as expected, as they will affect or see only the augmented part of the list,
- * not the base list. The only method that will access the base list and account for its indexes is get(). The only
- * method that should be used to modify this list is add().
+ * not the base list. The only methods that will access the base list and account for its indexes are get() and set().
+ * The only methods that should be used to modify this list are add() and set().
  */
 public class TIntAugmentedList extends TIntArrayList {
 
@@ -34,9 +34,22 @@ public class TIntAugmentedList extends TIntArrayList {
         if (index < startIndex) {
             return base.get(index);
         } else {
-            index -= startIndex;
-            return this.get(index);
+            return super.get(index - startIndex);
         }
+    }
+
+    @Override
+    public int set (int index, int value) {
+        if (index < startIndex) {
+            throw new RuntimeException("Modifying the base graph is not allowed.");
+        } else {
+            return super.set(index - startIndex, value);
+        }
+    }
+
+    @Override
+    public int size() {
+        return base.size() + super.size();
     }
 
 }

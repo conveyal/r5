4f554bec35a9468fe377d8c20271be444f85b5f1
Marko Burjek <s0mebody.slo@gmail.com>
2016-03-18 14:36:45 +0100

    Show itineraries in debug client only when clicked
    Otherwise they are hidden. This is possible without javascript with
    HTML5 details/summary elements.

--- src/main/resources/debug-plan/scripts/graphql_plan.js
+++ src/main/resources/debug-plan/scripts/graphql_plan.js
@@ -413,16 +413,16 @@ function makeTextResponse(data) {
     if (layer != null) {
         layer.clearLayers();
     }
-    var infos = "<ul>";
+    var infos = "";
     for(var i=0; i < options.length; i++) {
         var option = options[i];
-        var item = "<li>"+option.summary;
+        var item = "<details><summary>"+option.summary+"</summary>";
         var access = option.access;
         var egress = option.egress;
         var transit = option.transit;
         console.log("Summary:", option.summary);
         for(var j=0; j < option.itinerary.length; j++) {
-            item+="<br /><a href=\"#\" class=\"itinerary\" data-option=\""+i+"\" data-itinerary=\""+j+"\">Itinerary:</a>";
+            item+="<p><a href=\"#\" class=\"itinerary\" data-option=\""+i+"\" data-itinerary=\""+j+"\">Itinerary:</a></p>";
             item+="<ul>";
             var itinerary=option.itinerary[j];
             item+="<li>waitingTime: "+secondsToTime(itinerary.waitingTime)+"</li>";
@@ -472,7 +472,7 @@ function makeTextResponse(data) {
             item+="</ul>";
         }
         if (option.fares) {
-            item+="<br /><a>Fares:</a>";
+            item+="<p>Fares:</p>";
             for(var j=0; j < option.fares.length; j++) {
                 var fare = option.fares[j];
                 var unit = "";
@@ -480,7 +480,7 @@ function makeTextResponse(data) {
                 if (fare.currency == "USD") {
                     var symbol = " $";
                 }
-                item+="<br /><ul>";
+                item+="<ul>";
                 item+="<li>type: "+fare.type+"</li>";
                 item+="<li>low: "+fare.low.toFixed(2)+symbol+"</li>";
                 item+="<li>peak: "+fare.peak.toFixed(2)+symbol+"</li>";
@@ -490,7 +490,7 @@ function makeTextResponse(data) {
             }
         }
 
-        item+="</li>";
+        item+="</details>";
         infos+=item;
     }
     $(".response").append(infos+"</ul>");

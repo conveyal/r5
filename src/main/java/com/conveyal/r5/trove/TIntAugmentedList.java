0d5cf0db57d66267fc374ccd36cd48a27b2f9d5e
Andrew Byrd <andrew@fastmail.net>
2016-04-21 14:15:52 +0200

    Properly make TIntAugmentedList implement TIntList

--- src/main/java/com/conveyal/r5/trove/TIntAugmentedList.java
+++ src/main/java/com/conveyal/r5/trove/TIntAugmentedList.java
@@ -4,7 +4,6 @@ import gnu.trove.TIntCollection;
 import gnu.trove.function.TIntFunction;
 import gnu.trove.iterator.TIntIterator;
 import gnu.trove.list.TIntList;
-import gnu.trove.list.array.TIntArrayList;
 import gnu.trove.procedure.TIntProcedure;
 
 import java.util.Collection;
@@ -13,43 +12,324 @@ import java.util.Random;
 /**
  * This TIntArrayList extension wraps another TIntList, and only allows extending it and retrieving values.
  * It assumes the wrapped TIntList is immutable and treats it as such.
- * NOTE that most methods will not behave as expected, as they will affect or see only the augmented part of the list,
- * not the base list. The only methods that will access the base list and account for its indexes are get() and set().
- * The only methods that should be used to modify this list are add() and set().
  */
-public class TIntAugmentedList extends TIntArrayList {
+public class TIntAugmentedList implements TIntList {
 
     TIntList base;
 
-    int startIndex;
+    TIntList extension;
 
     public TIntAugmentedList(TIntList base) {
         super();
         this.base = base;
-        this.startIndex = base.size();
     }
 
     @Override
     public int get (int index) {
-        if (index < startIndex) {
+        if (index < base.size()) {
             return base.get(index);
         } else {
-            return super.get(index - startIndex);
+            return extension.get(index - base.size());
         }
     }
 
     @Override
     public int set (int index, int value) {
-        if (index < startIndex) {
+        if (index < base.size()) {
             throw new RuntimeException("Modifying the base graph is not allowed.");
         } else {
-            return super.set(index - startIndex, value);
+            return extension.set(index - base.size(), value);
         }
     }
 
     @Override
     public int size() {
-        return base.size() + super.size();
+        return base.size() + extension.size();
+    }
+
+    /**
+     *  Nominally implement the (enormous) TIntList interface.
+     *  But all of these remain unimplemented until we need them.
+     */
+
+    @Override
+    public void set(int offset, int[] values) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void set(int offset, int[] values, int valOffset, int length) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int replace(int offset, int val) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean remove(int value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> collection) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean containsAll(TIntCollection collection) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean containsAll(int[] array) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends Integer> collection) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(TIntCollection collection) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(int[] array) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> collection) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean retainAll(TIntCollection collection) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean retainAll(int[] array) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> collection) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeAll(TIntCollection collection) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeAll(int[] array) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int removeAt(int offset) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void remove(int offset, int length) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void transformValues(TIntFunction function) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void reverse() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void reverse(int from, int to) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void shuffle(Random rand) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public TIntList subList(int begin, int end) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int[] toArray() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int[] toArray(int offset, int len) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int[] toArray(int[] dest) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int[] toArray(int[] dest, int offset, int len) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int[] toArray(int[] dest, int source_pos, int dest_pos, int len) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean forEach(TIntProcedure procedure) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean forEachDescending(TIntProcedure procedure) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void sort() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void sort(int fromIndex, int toIndex) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void fill(int val) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void fill(int fromIndex, int toIndex, int val) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int binarySearch(int value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int binarySearch(int value, int fromIndex, int toIndex) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int indexOf(int value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int indexOf(int offset, int value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int lastIndexOf(int value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int lastIndexOf(int offset, int value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean contains(int value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public TIntIterator iterator() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public TIntList grep(TIntProcedure condition) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public TIntList inverseGrep(TIntProcedure condition) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int max() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int min() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int sum() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getNoEntryValue() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean add(int val) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void add(int[] vals) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void add(int[] vals, int offset, int length) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void insert(int offset, int value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void insert(int offset, int[] values) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void insert(int offset, int[] values, int valOffset, int len) {
+        throw new UnsupportedOperationException();
     }
 
 }

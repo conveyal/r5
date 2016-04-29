e79ab37c000e36cec73bc69f9cfe712316948d94
Matthew Wigginton Conway <matt@indicatrix.org>
2016-04-29 10:22:43 -0400

    test extending a route at the beginning.

--- src/test/java/com/conveyal/r5/analyst/scenario/AddStopsTest.java
+++ src/test/java/com/conveyal/r5/analyst/scenario/AddStopsTest.java
@@ -8,9 +8,11 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 import static com.conveyal.r5.analyst.scenario.FakeGraph.buildNetwork;
 import static com.conveyal.r5.analyst.scenario.FakeGraph.set;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -98,6 +100,44 @@ public class AddStopsTest {
             int[] d = schedule.departures;
             assertEquals(d[3] + 60, a[4]);
             assertEquals(a[4] + 40, d[4]);
+        }
+    }
+
+    /** Test adding stops to the beginning of a route, without removing any stops */
+    @Test
+    public void testExtendRouteAtStart () {
+        AddStops as = new AddStops();
+        // add s5 at start
+        as.toStop = "SINGLE_LINE:s1";
+        as.stops = Arrays.asList(new StopSpec("SINGLE_LINE:s5"));
+        as.dwellTimes = new int[] { 40 };
+        as.hopTimes = new int[] { 60 };
+        as.routes = set("SINGLE_LINE:route");
+
+        Scenario scenario = new Scenario();
+        scenario.modifications = Arrays.asList(as);
+
+        TransportNetwork mod = scenario.applyToTransportNetwork(network);
+
+        assertEquals(1, mod.transitLayer.tripPatterns.size());
+
+        TripPattern pattern = mod.transitLayer.tripPatterns.get(0);
+
+        // make sure the stops are in the right order
+        assertEquals(5, pattern.stops.length);
+        assertEquals("SINGLE_LINE:s5", mod.transitLayer.stopIdForIndex.get(pattern.stops[0]));
+        assertEquals("SINGLE_LINE:s1", mod.transitLayer.stopIdForIndex.get(pattern.stops[1]));
+        assertEquals("SINGLE_LINE:s2", mod.transitLayer.stopIdForIndex.get(pattern.stops[2]));
+        assertEquals("SINGLE_LINE:s3", mod.transitLayer.stopIdForIndex.get(pattern.stops[3]));
+        assertEquals("SINGLE_LINE:s4", mod.transitLayer.stopIdForIndex.get(pattern.stops[4]));
+
+        for (TripSchedule schedule : pattern.tripSchedules) {
+            int[] a = IntStream.of(schedule.arrivals).map(time -> time - schedule.arrivals[0]).toArray();
+            int[] d = IntStream.of(schedule.departures).map(time -> time - schedule.arrivals[0]).toArray();
+
+            // confirm the times are correct. Note that first and last stop of original route don't have a dwell time
+            assertArrayEquals(new int[] { 0, 100, 600, 1130, 1660 }, a);
+            assertArrayEquals(new int[] { 40, 100, 630, 1160, 1660}, d);
         }
     }
 

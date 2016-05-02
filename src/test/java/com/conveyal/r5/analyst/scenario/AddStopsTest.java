16c4e1b555aed0d0a181ff79748e718661cee753
Matthew Wigginton Conway <matt@indicatrix.org>
2016-05-02 16:45:53 -0400

    update add stops test after conversation with @abyrd. build will succeed once modification class is updated.

--- src/test/java/com/conveyal/r5/analyst/scenario/AddStopsTest.java
+++ src/test/java/com/conveyal/r5/analyst/scenario/AddStopsTest.java
@@ -36,7 +36,7 @@ public class AddStopsTest {
         as.fromStop = "SINGLE_LINE:s2";
         as.toStop = "SINGLE_LINE:s4";
         as.stops = Arrays.asList(new StopSpec("SINGLE_LINE:s5"));
-        as.dwellTimes = new int[] { 40 };
+        as.dwellTimes = new int[] { 20, 40, 50 };
         as.hopTimes = new int[] { 60, 70 };
         as.routes = set("SINGLE_LINE:route");
 
@@ -64,11 +64,11 @@ public class AddStopsTest {
             assertEquals("SINGLE_LINE:trip" + schedule.arrivals[0], schedule.tripId);
 
             // confirm the times are correct. Note that first and last stop of original route don't have a dwell time
-            // so this is no dwell at s1, 500 sec travel time (from FakeGraph) to s2, 30 sec dwell time at s2,
+            // so this is no dwell at s1, 500 sec travel time (from FakeGraph) to s2, 20 sec dwell time at s2 (overwritten by modification),
             // 60 sec travel time to s5 (which replaces s3), 40 sec dwell at s5, 70 sec travel time to s4 (which is part
-            // of the original route), and 0 sec dwell time at s4
-            assertArrayEquals(new int[] { 0, 500, 590, 700 }, a);
-            assertArrayEquals(new int[] { 0, 530, 630, 700 }, d);
+            // of the original route), and 50 sec dwell time at s4, replaces 0 from original modification
+            assertArrayEquals(new int[] { 0, 500, 580, 690 }, a);
+            assertArrayEquals(new int[] { 0, 520, 620, 740 }, d);
         }
     }
 
@@ -82,7 +82,7 @@ public class AddStopsTest {
         // add s5 at beginning
         as.toStop = "SINGLE_LINE:s1";
         as.stops = Arrays.asList(new StopSpec("SINGLE_LINE:s5"));
-        as.dwellTimes = new int[] { 40 };
+        as.dwellTimes = new int[] { 40, 50 };
         as.hopTimes = new int[] { 60 };
         as.routes = set("SINGLE_LINE:route");
 
@@ -105,10 +105,16 @@ public class AddStopsTest {
 
         for (TripSchedule schedule : pattern.tripSchedules) {
             // confirm the times are correct
-            int[] a = schedule.arrivals;
-            int[] d = schedule.departures;
-            assertEquals(d[0] - 40, a[0]);
-            assertEquals(a[1] - 60, d[0]);
+            int[] a = IntStream.of(schedule.arrivals).map(time -> time - schedule.arrivals[0]).toArray();
+            int[] d = IntStream.of(schedule.departures).map(time -> time - schedule.arrivals[0]).toArray();
+
+            // slightly awkward, but make sure that the trip passes the original first stop at the same time it did before
+            assertEquals("SINGLE_LINE:trip" + schedule.arrivals[1], schedule.tripId);
+
+            // 40 sec dwell time at added stop s5, 60 sec travel time to s1, 50 sec dwell at s1, and back to 500 and 30 sec dwell time
+            // to end (with s4 having no dwell, per FakeGraph)
+            assertArrayEquals(new int[] { 0, 100, 650, 1180, 1710 }, a);
+            assertArrayEquals(new int[] { 40, 150, 680, 1210, 1710 }, d);
         }
     }
 
@@ -122,7 +128,7 @@ public class AddStopsTest {
         // add s5 at end
         as.fromStop = "SINGLE_LINE:s4";
         as.stops = Arrays.asList(new StopSpec("SINGLE_LINE:s5"));
-        as.dwellTimes = new int[] { 40 };
+        as.dwellTimes = new int[] { 20, 40 };
         as.hopTimes = new int[] { 60 };
         as.routes = set("SINGLE_LINE:route");
 
@@ -152,56 +158,10 @@ public class AddStopsTest {
 
             // confirm the times are correct. Note that first and last stop of original route don't have a dwell time
             // so 0 sec dwell time at s1, 500 sec travel time from FakeGraph to s2, 30 sec dwell time at s2, 500 sec
-            // travel time to s3, 30 sec dwell time at s3, 500 sec travel time to s4, 0 sec dwell time at s4, and 60 sec travel
+            // travel time to s3, 30 sec dwell time at s3, 500 sec travel time to s4, 20 sec dwell time at s4 from modification, and 60 sec travel
             // time and 40 sec dwell time from modification to added stop s5
-            assertArrayEquals(new int[] { 0, 500, 1030, 1560, 1620 }, a);
-            assertArrayEquals(new int[] { 0, 530, 1060, 1560, 1660}, d);
-        }
-    }
-
-    /** Test adding stops to the beginning of a route, without removing any stops */
-    @Test
-    public void testExtendRouteAtStart () {
-        AddStops as = new AddStops();
-        // add s5 at start
-        as.toStop = "SINGLE_LINE:s1";
-        as.stops = Arrays.asList(new StopSpec("SINGLE_LINE:s5"));
-        as.dwellTimes = new int[] { 40 };
-        as.hopTimes = new int[] { 60 };
-        as.routes = set("SINGLE_LINE:route");
-
-        Scenario scenario = new Scenario();
-        scenario.modifications = Arrays.asList(as);
-
-        TransportNetwork mod = scenario.applyToTransportNetwork(network);
-
-        assertEquals(1, mod.transitLayer.tripPatterns.size());
-
-        TripPattern pattern = mod.transitLayer.tripPatterns.get(0);
-
-        // make sure the stops are in the right order
-        assertEquals(5, pattern.stops.length);
-        assertEquals("SINGLE_LINE:s5", mod.transitLayer.stopIdForIndex.get(pattern.stops[0]));
-        assertEquals("SINGLE_LINE:s1", mod.transitLayer.stopIdForIndex.get(pattern.stops[1]));
-        assertEquals("SINGLE_LINE:s2", mod.transitLayer.stopIdForIndex.get(pattern.stops[2]));
-        assertEquals("SINGLE_LINE:s3", mod.transitLayer.stopIdForIndex.get(pattern.stops[3]));
-        assertEquals("SINGLE_LINE:s4", mod.transitLayer.stopIdForIndex.get(pattern.stops[4]));
-
-        for (TripSchedule schedule : pattern.tripSchedules) {
-            int[] a = IntStream.of(schedule.arrivals).map(time -> time - schedule.arrivals[0]).toArray();
-            int[] d = IntStream.of(schedule.departures).map(time -> time - schedule.arrivals[0]).toArray();
-
-            // slightly awkward, but make sure that the trip starts at the same time it did before
-            // (the remainder of the schedule should just be pushed out); the original start time is part of the trip ID,
-            // which is not changed by the modification
-            assertEquals("SINGLE_LINE:trip" + schedule.arrivals[0], schedule.tripId);
-
-            // confirm the times are correct. Note that first and last stop of original route don't have a dwell time
-            // so from modification, 40 sec dwell time at added stop s5, 60 sec travel time to s1, and then continue original
-            // schedule: 0 sec dwell time at s1, 500 sec travel time from FakeGraph to s2, 30 sec dwell time at s2, 500 sec
-            // travel time to s3, 30 sec dwell time at s3, 500 sec travel time to s4, 0 sec dwell time at s4
-            assertArrayEquals(new int[] { 0, 100, 600, 1130, 1660 }, a);
-            assertArrayEquals(new int[] { 40, 100, 630, 1160, 1660}, d);
+            assertArrayEquals(new int[] { 0, 500, 1030, 1560, 1640 }, a);
+            assertArrayEquals(new int[] { 0, 530, 1060, 1580, 1680}, d);
         }
     }
 

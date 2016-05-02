8ee37a1a62518fad83a39a37e14e8ea11a3857ec
Matthew Wigginton Conway <matt@indicatrix.org>
2016-05-02 17:28:37 -0400

    insert stop in middle without removing any stops, and test stop adding.

--- src/test/java/com/conveyal/r5/analyst/scenario/AddStopsTest.java
+++ src/test/java/com/conveyal/r5/analyst/scenario/AddStopsTest.java
@@ -1,8 +1,11 @@
 package com.conveyal.r5.analyst.scenario;
 
+import com.conveyal.r5.streets.VertexStore;
 import com.conveyal.r5.transit.TransportNetwork;
 import com.conveyal.r5.transit.TripPattern;
 import com.conveyal.r5.transit.TripSchedule;
+import gnu.trove.list.TIntList;
+import gnu.trove.map.TIntIntMap;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,6 +17,8 @@ import static com.conveyal.r5.analyst.scenario.FakeGraph.buildNetwork;
 import static com.conveyal.r5.analyst.scenario.FakeGraph.set;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Test adding stops (rerouting)
@@ -166,6 +171,80 @@ public class AddStopsTest {
             // time and 40 sec dwell time from modification to added stop s5
             assertArrayEquals(new int[] { 0, 500, 1030, 1560, 1640 }, a);
             assertArrayEquals(new int[] { 0, 530, 1060, 1580, 1680}, d);
+        }
+
+        assertEquals(checksum, network.checksum());
+    }
+
+    /** Insert a (created) stop in the middle of a route without removing any existing stops */
+    @Test
+    public void insertStopInMiddle () {
+        AddStops as = new AddStops();
+        as.stops = Arrays.asList(
+                new StopSpec(-83.007, 39.967)
+        );
+
+        as.fromStop = "SINGLE_LINE:s2";
+        as.toStop = "SINGLE_LINE:s3";
+        as.hopTimes = new int[] { 30, 40 };
+        as.dwellTimes = new int[] { 10, 15, 20 };
+        as.routes = set("SINGLE_LINE:route");
+
+        Scenario scenario = new Scenario();
+        scenario.modifications = Arrays.asList(as);
+
+        TransportNetwork mod = scenario.applyToTransportNetwork(network);
+
+        assertEquals(1, mod.transitLayer.tripPatterns.size());
+
+        TripPattern pattern = mod.transitLayer.tripPatterns.get(0);
+
+        // make sure the stops are in the right order
+        assertEquals(5, pattern.stops.length);
+        assertEquals("SINGLE_LINE:s1", mod.transitLayer.stopIdForIndex.get(pattern.stops[0]));
+        assertEquals("SINGLE_LINE:s2", mod.transitLayer.stopIdForIndex.get(pattern.stops[1]));
+        assertEquals(null, mod.transitLayer.stopIdForIndex.get(pattern.stops[2]));
+        assertEquals("SINGLE_LINE:s3", mod.transitLayer.stopIdForIndex.get(pattern.stops[3]));
+        assertEquals("SINGLE_LINE:s4", mod.transitLayer.stopIdForIndex.get(pattern.stops[4]));
+
+        // confirm that the inserted stop is in the right place and has stop trees and transfers
+        int sidx = pattern.stops[2];
+        int vidx = mod.transitLayer.streetVertexForStop.get(sidx);
+        assertTrue(vidx >= 0);
+
+        assertTrue(network.streetLayer.getVertexCount() <= vidx); // vertex should not be present in original network
+
+        VertexStore.Vertex v = mod.streetLayer.vertexStore.getCursor(vidx);
+        assertEquals(-83.007, v.getLon(), 1e-6);
+        assertEquals(39.967, v.getLat(), 1e-6);
+
+        // does it have a stop tree?
+        TIntIntMap stopTree = mod.transitLayer.stopTrees.get(sidx);
+        assertNotNull(stopTree);
+        // jagged array, should reach more than 10 vertices
+        assertTrue(stopTree.size() > 20);
+
+        // does it have transfers?
+        TIntList transfers = mod.transitLayer.transfersForStop.get(sidx);
+        assertNotNull(transfers);
+        // transfers is a jagged array, should have at least one stop
+        assertTrue(transfers.size() >= 2);
+
+        assertEquals(1, mod.transitLayer.tripPatterns.size());
+
+        for (TripSchedule schedule : pattern.tripSchedules) {
+            // slightly awkward, but make sure that the trip starts at the same time it did before
+            assertEquals("SINGLE_LINE:trip" + schedule.arrivals[0], schedule.tripId);
+
+            int[] a = IntStream.of(schedule.arrivals).map(time -> time - schedule.arrivals[0]).toArray();
+            int[] d = IntStream.of(schedule.departures).map(time -> time - schedule.arrivals[0]).toArray();
+
+            // confirm the times are correct. Note that first and last stop of original route don't have a dwell time
+            // so 0 sec dwell time at s1, 500 sec travel time from FakeGraph to s2, 10 sec dwell time at s2 from modification,
+            // 30 sec travel time to added stop, 15 sec dwell time at added stop, 40 sec travel time to s3, 20 sec dwell time
+            // at s3, and back to original 500 sec travel time to s4 and 0 sec dwell time at s4, from FakeGraph
+            assertArrayEquals(new int[] { 0, 500, 540, 595, 1115, 1645 }, a);
+            assertArrayEquals(new int[] { 0, 510, 555, 615, 1145, 1645 x}, d);
         }
 
         assertEquals(checksum, network.checksum());

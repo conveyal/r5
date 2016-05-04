477f748d25e164a34338f6abea32202d65b30f8c
Matthew Wigginton Conway <matt@indicatrix.org>
2016-05-04 16:35:05 -0400

    fix #150.

--- src/test/java/com/conveyal/r5/analyst/scenario/AdjustSpeedTest.java
+++ src/test/java/com/conveyal/r5/analyst/scenario/AdjustSpeedTest.java
@@ -13,6 +13,7 @@ import static com.conveyal.r5.analyst.scenario.FakeGraph.buildNetwork;
 import static com.conveyal.r5.analyst.scenario.FakeGraph.set;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -35,8 +36,6 @@ public class AdjustSpeedTest {
 
         TripSchedule exemplarSchedule = network.transitLayer.tripPatterns.get(0).tripSchedules.get(0);
 
-        int baseTripLength = exemplarSchedule.departures[exemplarSchedule.departures.length - 1] - exemplarSchedule.arrivals[0];
-
         AdjustSpeed as = new AdjustSpeed();
         as.routes = set("SINGLE_LINE:route");
         as.scale = 2;
@@ -49,9 +48,20 @@ public class AdjustSpeedTest {
 
         assertEquals(1, mod.transitLayer.tripPatterns.size());
 
+        // make sure that there still are trip patterns
+        assertFalse(mod.transitLayer.tripPatterns.get(0).tripSchedules.isEmpty());
+
         for (TripSchedule schedule : mod.transitLayer.tripPatterns.get(0).tripSchedules) {
-            int newTripLength = schedule.departures[schedule.departures.length - 1] - schedule.arrivals[0];
-            assertEquals(baseTripLength / 2, newTripLength, 2); // epsilon of 2 seconds to account for rounding errors
+            int[] a = IntStream.of(schedule.arrivals).map(time -> time - schedule.arrivals[0]).toArray();
+            int[] d = IntStream.of(schedule.departures).map(time -> time - schedule.arrivals[0]).toArray();
+
+            // slightly awkward, but make sure that the trip starts at the same time as it did before
+            // TODO user-settable zero point for modification
+            assertEquals("SINGLE_LINE:trip" + schedule.arrivals[0], schedule.tripId);
+
+            // was 500 sec travel, 30 sec dwell, now 250 sec travel and 15 sec dwell
+            assertArrayEquals(new int[] { 0, 250, 515, 780 }, a);
+            assertArrayEquals(new int[] { 0, 265, 530, 780 }, d);
         }
 
         assertEquals(checksum, network.checksum());
@@ -81,6 +91,23 @@ public class AdjustSpeedTest {
             int newTripLength = schedule.departures[schedule.departures.length - 1] - schedule.arrivals[0];
             assertEquals(expectedTripLength, newTripLength, 2); // epsilon of 2 seconds to account for rounding errors
         }
+
+        // make sure that there still are trip patterns
+        assertFalse(mod.transitLayer.tripPatterns.get(0).tripSchedules.isEmpty());
+
+        for (TripSchedule schedule : mod.transitLayer.tripPatterns.get(0).tripSchedules) {
+            int[] a = IntStream.of(schedule.arrivals).map(time -> time - schedule.arrivals[0]).toArray();
+            int[] d = IntStream.of(schedule.departures).map(time -> time - schedule.arrivals[0]).toArray();
+
+            // slightly awkward, but make sure that the trip starts at the same time as it did before
+            // TODO user-settable zero point for modification
+            assertEquals("SINGLE_LINE:trip" + schedule.arrivals[0], schedule.tripId);
+
+            // was 500 sec travel, 30 sec dwell, now 250 sec travel and still 30 sec dwell
+            assertArrayEquals(new int[] { 0, 250, 530, 810 }, a);
+            assertArrayEquals(new int[] { 0, 280, 560, 810 }, d);
+        }
+
 
         assertEquals(checksum, network.checksum());
     }
@@ -167,6 +194,29 @@ public class AdjustSpeedTest {
 
         assertTrue(mod.transitLayer.tripPatterns.get(0).tripSchedules.stream()
                 .anyMatch(schedule -> !"SINGLE_LINE:trip25200".equals(schedule.tripId)));
+
+        // make sure that there still are trip patterns
+        assertFalse(mod.transitLayer.tripPatterns.get(0).tripSchedules.isEmpty());
+
+        for (TripSchedule schedule : mod.transitLayer.tripPatterns.get(0).tripSchedules) {
+            int[] a = IntStream.of(schedule.arrivals).map(time -> time - schedule.arrivals[0]).toArray();
+            int[] d = IntStream.of(schedule.departures).map(time -> time - schedule.arrivals[0]).toArray();
+
+            // slightly awkward, but make sure that the trip starts at the same time as it did before
+            // TODO user-settable zero point for modification
+            assertEquals("SINGLE_LINE:trip" + schedule.arrivals[0], schedule.tripId);
+
+            if ("SINGLE_LINE:trip25200".equals(schedule.tripId)) {
+                // was 500 sec travel, 30 sec dwell, now 250 sec travel and 15 sec dwell
+                assertArrayEquals(new int[]{0, 250, 515, 780}, a);
+                assertArrayEquals(new int[]{0, 265, 530, 780}, d);
+            } else {
+                // not modified
+                assertArrayEquals(new int[]{0, 500, 1030, 1560}, a);
+                assertArrayEquals(new int[]{0, 530, 1060, 1560}, d);
+            }
+        }
+
 
         assertEquals(checksum, network.checksum());
     }

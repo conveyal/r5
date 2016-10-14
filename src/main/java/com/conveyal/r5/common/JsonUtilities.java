e3a6d7729d260e962f4e5df64ce1e3f2b5e83c23
Matthew Wigginton Conway <matt@indicatrix.org>
2016-10-14 08:56:26 -0400

    add lenientObjectMapper that does not fail on missing fields.

--- src/main/java/com/conveyal/r5/common/JsonUtilities.java
+++ src/main/java/com/conveyal/r5/common/JsonUtilities.java
@@ -11,14 +11,22 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  * Created by matthewc on 10/23/15.
  */
 public class JsonUtilities {
-    public static final ObjectMapper objectMapper = new ObjectMapper();
+    public static final ObjectMapper objectMapper = createBaseObjectMapper();
+
     static {
+        // If we receive a JSON object containing a field that we don't recognize, fail. This should catch misspellings.
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+    }
+
+    public static final ObjectMapper lenientObjectMapper = createBaseObjectMapper();
+
+    private static ObjectMapper createBaseObjectMapper () {
+        ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(JavaLocalDateSerializer.makeModule());
         objectMapper.registerModule(new GeoJsonModule());
         objectMapper.registerModule(BitSetSerializer.makeModule());
         objectMapper.configure(JsonParser.Feature.ALLOW_COMMENTS, true);
         objectMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
-        // If we receive a JSON object containing a field that we don't recognize, fail. This should catch misspellings.
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+        return objectMapper;
     }
 }

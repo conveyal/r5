6bbdcfcabf04d6ad1b8673ceb595d8bbe398cb7e
Andrew Byrd <andrew@fastmail.net>
2016-10-24 17:04:08 +0800

    strict travel time dominance for all states with more rounds than existing states
    this should speed up search significantly. the exact dominance function may need revision, this is just a test.

--- src/main/java/com/conveyal/r5/profile/SuboptimalDominatingList.java
+++ src/main/java/com/conveyal/r5/profile/SuboptimalDominatingList.java
@@ -19,10 +19,12 @@ public class SuboptimalDominatingList implements DominatingList {
 
     private List<McRaptorSuboptimalPathProfileRouter.McRaptorState> list = new LinkedList<>();
 
-    public boolean add (McRaptorSuboptimalPathProfileRouter.McRaptorState state) {
-        // is this state dominated?
-        if (bestTime != Integer.MAX_VALUE && bestTime + suboptimalSeconds < state.time)
+    public boolean add (McRaptorSuboptimalPathProfileRouter.McRaptorState newState) {
+
+        // If the new state is later than the best known time plus the suboptimality parameter, then throw it away.
+        if (bestTime != Integer.MAX_VALUE && bestTime + suboptimalSeconds < newState.time) {
             return false;
+        }
 
         // apply strict dominance if there is a state at the previous round on the same previous pattern arriving at this
         // stop (prevents reboarding/hopping between routes on common trunks)
@@ -49,28 +51,35 @@ public class SuboptimalDominatingList implements DominatingList {
         // We only look back one pattern; the reason for this is that we want to avoid a lot of looping in a function
         // that gets called a lot, and it seems unlikely that there would be time to take two other patterns and still
         // slip into the window of suboptimality. I haven't tested it though to see its effect on response times.
-        if (state.pattern != -1 && state.patterns.length > 1) {
-            for (McRaptorSuboptimalPathProfileRouter.McRaptorState s : list) {
-                if (s.round == state.round - 1 && s.pattern == state.patterns[s.round - 1] && s.time <= state.time) {
-                    return false;
-                }
-            }
+//        if (state.pattern != -1 && state.patterns.length > 1) {
+//            for (McRaptorSuboptimalPathProfileRouter.McRaptorState s : list) {
+//                if (s.round == state.round - 1 && s.pattern == state.patterns[s.round - 1] && s.time <= state.time) {
+//                    return false;
+//                }
+//            }
+//        }
+
+        // If there is any way to reach this location with less rides and the same or less time, throw away the new state.
+        for (McRaptorSuboptimalPathProfileRouter.McRaptorState oldState : list) {
+            if (oldState.round < newState.round && oldState.time <= newState.time) return false;
         }
 
-        if (state.time < bestTime) bestTime = state.time;
+        // Update the best time at this location to reflect the new state.
+        if (newState.time < bestTime) bestTime = newState.time;
 
-        // don't forget this
-        list.add(state);
+        // The new state is non-dominated. Keep it.
+        list.add(newState);
 
         return true;
     }
 
-    /** prune dominated and excessive states */
+    /** Prune dominated and excessive states. */
     public void prune () {
         // group states that have the same sequence of patterns, throwing out dominated states as we go
         for (Iterator<McRaptorSuboptimalPathProfileRouter.McRaptorState> it = list.iterator(); it.hasNext();) {
-            if (it.next().time >= bestTime + suboptimalSeconds)
+            if (it.next().time >= bestTime + suboptimalSeconds) {
                 it.remove();
+            }
         }
     }
 

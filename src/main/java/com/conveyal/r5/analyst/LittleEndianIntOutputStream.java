75547378cc41e8e3080e5762b0df79da9075cd5a
Matthew Wigginton Conway <matt@indicatrix.org>
2016-12-09 12:30:31 -0500

    add writeChars method to LittleEndianIntOutputStream.

--- src/main/java/com/conveyal/r5/analyst/LittleEndianIntOutputStream.java
+++ src/main/java/com/conveyal/r5/analyst/LittleEndianIntOutputStream.java
@@ -3,6 +3,7 @@ package com.conveyal.r5.analyst;
 import java.io.FilterOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.charset.Charset;
 
 /**
  * Class to output ints. An order of magnitude faster than Guava LittleEndianDataOutputStream.
@@ -20,6 +21,12 @@ public class LittleEndianIntOutputStream extends FilterOutputStream {
                 (byte) (value >> 24 & 0xff)
         };
 
+        out.write(bytes);
+    }
+
+    /** Write an ASCII string as bytes, generally used as a header */
+    public void writeAscii(String accessgr) throws IOException {
+        byte[] bytes = accessgr.getBytes(Charset.forName("ASCII"));
         out.write(bytes);
     }
 }

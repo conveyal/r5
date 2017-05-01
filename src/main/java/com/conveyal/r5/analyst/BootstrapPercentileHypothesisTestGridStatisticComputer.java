2e87b4082d6e27d372d837641ff809c7b91865a1
Matthew Wigginton Conway <matt@indicatrix.org>
2017-05-01 10:52:43 -0400

    Clean up bootstrap hypothesis test code.

--- src/main/java/com/conveyal/r5/analyst/BootstrapPercentileHypothesisTestGridStatisticComputer.java
+++ src/main/java/com/conveyal/r5/analyst/BootstrapPercentileHypothesisTestGridStatisticComputer.java
@@ -5,15 +5,20 @@ package com.conveyal.r5.analyst;
  * percentile of interest, land use, time window, etc.) rather than due to random variation from the Monte Carlo search
  * process. This uses the confidence interval method of bootstrap hypothesis testing described on page 214 of Efron and
  * Tibshirani (1993). We perform a two-tailed test, so we are not making an assumptions about whether the change was positive
- * or negative.
+ * or negative; a significant value indicates that there was a change, and that the sign is correct.
  *
  * This technique works by constructing a confidence interval that has one end at 0 and the other end where it may lie to
  * make the confidence interval symmetric (i.e. the same amount of density in both tails). We use the percentile method
  * to calculate this confidence interval; while we are aware there are issues with this, the better bootstrap confidence
  * interval methods require us to store additional data so we can compute a jackknife estimate of acceleration (Efron
- * and Tishirani 1993, ch. 14).
+ * and Tibshirani 1993, ch. 14).
  *
- * TODO reread this section of Efron and Tibshirana (1993) to be sure I understand it when I have the book handy.
+ * While it might initially seem that we could use the permutation test described earlier in that chapter, we in fact cannot.
+ * It attractively promises to compare two distributions, which is exactly what we have, but it promises to compare some
+ * statistic of those distributions. We're not interested in the difference in means of the sampling distributions, we're
+ * interested in the the difference of the full sampling distribution. To use the permutation test, we would have to apply
+ * it one level up the stack, when computing the regional results, and compute two regional runs simultaneously so we could
+ * permute them when we still had the travel times for all iterations in memory.
  *
  * References
  * Efron, B., & Tibshirani, R. J. (1993). An Introduction to the Bootstrap. Boca Raton, FL: Chapman and Hall/CRC.
@@ -48,11 +53,17 @@ public class BootstrapPercentileHypothesisTestGridStatisticComputer extends Dual
         }
 
         double pVal;
-        // TODO should we be looking at the point estimate here at all?
+
+        // if the point estimate was less than zero, assume the confidence interval is less than zero
+        // This could be wrong if the accessibility does not lie on the same side of zero as the majority of the density.
+        // TODO Efron and Tibshirani don't really discuss how to handle that case, and in particular don't discuss two-
+        // tailed tests at all. 
         if (pointEstimate < 0) {
-            // compute the density that lies at or above zero
+            // compute the density that lies at or above zero. We have changed the technique slightly from what is
+            // described in Efron and Tibshirani 1993 to explicitly handle values that are exactly 0 (important because
+            // we are working with discretized, integer data).
             double densityAtOrAboveZero = (double) (nZero + nAboveZero) / nTotal;
-            // two tailed test, take this much density off the other end as well, and that's the alpha for this test.
+            // two tailed test, take this much density off the other end as well, and that's the p-value for this test.
             pVal = 2 * densityAtOrAboveZero;
         } else {
             // compute the density that lies at or below zero
@@ -61,9 +72,14 @@ public class BootstrapPercentileHypothesisTestGridStatisticComputer extends Dual
             pVal = 2 * densityAtOrBelowZero;
         }
 
+        // fix up case where point est does not lie on same side of 0 as majority of density.
+        if (pVal > 1) pVal = 1;
+
         // scale probability to 0 - 100,000
-        // note that the return value can actually be greater than 100,000 if a majority of the density lies on the
+        // note that the return value can actually be less than 0 if a majority of the density lies on the
         // opposite side of zero from the point estimate. Maybe we shouldn't be looking at the point estimate
-        return pVal * 1e5;
+        // NB for historical reasons, the probability grids show the probability of change, not the probability of no
+        // change (i.e. the p-value). Invert the values (i.e. replace p with alpha)
+        return (1 - pVal) * 1e5;
     }
 }

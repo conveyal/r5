820815ef5389fdb0172fb7a0dccab667f535c720
ansons <ansonstewart@gmail.com>
2017-09-27 13:36:33 -0400

    docs(pointsets) edit comment
    explain why our handling of points outside the transit network (namely marking them as unlinked) is appropriate

--- src/main/java/com/conveyal/r5/analyst/WebMercatorGridPointSet.java
+++ src/main/java/com/conveyal/r5/analyst/WebMercatorGridPointSet.java
@@ -60,6 +60,10 @@ public class WebMercatorGridPointSet extends PointSet implements Serializable {
         // TODO don't copy, just have a cache that lazy loads and crops from base pointset
         if (base != null) {
             base.linkageCache.asMap().forEach((key, baseLinkage) -> {
+                // LinkedPointSet now handles the case where the new grid is not completely contained by the base grid.
+                // Since this generally happens when there are points beyond the transit network's extents, marking the points
+                // that are not contained by the base linkage as unlinked (and logging a warning if all points are beyond
+                // the network) is sufficient as you will not be able to reach these locations anyhow.
                 LinkedPointSet croppedLinkage = new LinkedPointSet(baseLinkage, this);
                 this.linkageCache.put(key, croppedLinkage);
             });

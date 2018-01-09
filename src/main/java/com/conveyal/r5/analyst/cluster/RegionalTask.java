c974ff2913c8fcf27cf41448d8a02f44e9456c70
Andrew Byrd <andrew@fastmail.net>
2018-01-09 18:26:20 +0100

    Supply full-region destination grid when making static sites
    Though they are part of a regional job, tasks for a static site need to save travel times to all destinations, independent of any one destination grid.

--- src/main/java/com/conveyal/r5/analyst/cluster/RegionalTask.java
+++ src/main/java/com/conveyal/r5/analyst/cluster/RegionalTask.java
@@ -56,24 +56,35 @@ public class RegionalTask extends AnalysisTask implements Cloneable {
         return false; // regional analysis tasks are not high priority
     }
 
-    /** Regional analyses use the extents of the destination opportunity grids as their destination extents.
+    /**
+     * Regional analyses use the extents of the destination opportunity grids as their destination extents.
      * We don't want to enqueue duplicate tasks with the same destination pointset extents, because it is more efficient
-     * to compute travel time for a given destination only once, then accumulate multiple accessibility values for multiple
-     * opportunities at that destination*/
-
+     * to compute travel time for a given destination only once, then accumulate multiple accessibility values
+     * for multiple opportunities at that destination.
+     */
     @Override
     public List<PointSet> getDestinations(TransportNetwork network, GridCache gridCache) {
         List<Grid> gridList = new ArrayList<>();
         List<PointSet> pointSets = new ArrayList<>();
 
-        //TODO check that this actually works and clean it up
-        if (grid != null){ // single grid specified
+        if (makeStaticSite) {
+            // In the special case where we're making a static site, a regional task is producing travel time grids.
+            // This is unlike the usual case where regional tasks produce accessibility indicator values.
+            // Because the time grids are not intended for one particular set of destinations,
+            // they should cover the whole analysis region.
+            // FIXME this limits the destination grid bounds to be exactly those of the origin grid.
+            pointSets.add(gridPointSetCache.get(this.zoom, this.west, this.north, this.width, this.height, network.gridPointSet));
+            return pointSets;
+        }
 
+        if (grid != null){
+            // A single grid is specified.
             gridData = gridCache.get(grid);
             pointSets.add(gridPointSetCache.get(gridData, network.gridPointSet));
-
-        } else { // grids specified; add only the first one, and any with different extents, to pointSets
-
+        } else {
+            // Multiple grids should be specified. Add only the first one, and any with different extents, to pointSets.
+            // TODO more explanation, complete implementation. This block is currently unused.
+            // FIXME we really shouldn't have two different implementations present, one for a list and one for a single grid.
             gridData = gridCache.get(grids.get(0));
             gridList.add(gridData);
             pointSets.add(gridPointSetCache.get(gridData, network.gridPointSet));

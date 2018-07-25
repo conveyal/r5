93cb50ae9fc0f3b1690592a7af46ba3392c8d69d
Matthew Wigginton Conway <matt@indicatrix.org>
2018-07-25 17:02:06 -0400

    feat(fares): implement proper algorithm for non-greedy fare calculation.

--- src/main/java/com/conveyal/r5/profile/FareDominatingList.java
+++ src/main/java/com/conveyal/r5/profile/FareDominatingList.java
@@ -1,5 +1,6 @@
 package com.conveyal.r5.profile;
 
+import com.conveyal.r5.analyst.fare.FareBounds;
 import com.conveyal.r5.analyst.fare.InRoutingFareCalculator;
 
 import java.util.Collection;
@@ -18,27 +19,42 @@ public class FareDominatingList implements DominatingList {
         this.fareCalculator = fareCalculator;
     }
 
+    /** Is dominator strictly better than dominatee? */
+    private boolean dominates (McRaptorSuboptimalPathProfileRouter.McRaptorState dominator, McRaptorSuboptimalPathProfileRouter.McRaptorState dominatee) {
+        // apply strict dominance if fares and transfer privileges are same
+        FareBounds dominatorFare = fareCalculator.calculateFare(dominator);
+        FareBounds dominateeFare = fareCalculator.calculateFare(dominatee);
+        if (dominatorFare.cumulativeFarePaid == dominateeFare.cumulativeFarePaid &&
+                dominatorFare.transferPrivilege.maxValue == dominateeFare.transferPrivilege.maxValue &&
+                dominatorFare.expirationTime >= dominateeFare.expirationTime &&
+                dominatorFare.numberLimit >= dominateeFare.numberLimit) {
+            return dominator.time < dominatee.time;
+        } else {
+            // non-strict dominance
+            int dominateeConsumedValue = dominateeFare.cumulativeFarePaid - dominateeFare.transferPrivilege.maxValue;
+            // if you can get to this location using the dominator for less than the "consumed cost" -- i.e. the fare
+            // minus any potential discounts -- of the dominatee, the dominatee is dominated.
+            return dominator.time < dominatee.time && dominatorFare.cumulativeFarePaid < dominateeConsumedValue;
+        }
+    }
+
     @Override
-    public boolean add(McRaptorSuboptimalPathProfileRouter.McRaptorState state) {
-        int thisFare = fareCalculator.calculateFare(state).getFarePaid();
-
+    public boolean add(McRaptorSuboptimalPathProfileRouter.McRaptorState newState) {
         for (Iterator<McRaptorSuboptimalPathProfileRouter.McRaptorState> it = states.iterator(); it.hasNext();) {
-            McRaptorSuboptimalPathProfileRouter.McRaptorState other = it.next();
-
-            int otherFare = fareCalculator.calculateFare(other).getFarePaid();
+            McRaptorSuboptimalPathProfileRouter.McRaptorState existing = it.next();
 
             // using geq/leq below to avoid codominant states in the same place on the pareto curve
-            if (other.time <= state.time && otherFare <= thisFare) {
+            if (dominates(existing, newState)) {
                 return false;
             }
 
-            if (other.time >= state.time && otherFare >= thisFare) {
+            if (dominates(newState, existing)) {
                 it.remove();
             }
         }
 
         // if we haven't returned false by now, state is nondominated.
-        states.add(state);
+        states.add(newState);
         return true;
     }
 

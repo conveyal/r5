f1947ffd6d829e645a1db7644668fa11cbd03b27
Thomas Gran <t2gran@gmail.com>
2019-03-31 16:19:23 +0200

    Move the 'speed_test' package to 'com.conveyal.r5.otp2.speed_test'.

--- src/main/java/com/conveyal/r5/speed_test/SpeedTestProfileRequest.java
+++
@@ -1,8 +0,0 @@
-package com.conveyal.r5.speed_test;
-
-import com.conveyal.r5.profile.ProfileRequest;
-
-public class SpeedTestProfileRequest extends ProfileRequest {
-    public String description;
-    public int numberOfItineraries;
-}
--- src/main/java/com/conveyal/r5/speed_test/test/NoResultFound.java
+++
@@ -1,8 +0,0 @@
-package com.conveyal.r5.speed_test.test;
-
-
-public class NoResultFound extends TestCaseFailedException {
-    public NoResultFound() {
-        super("NO RESULT FOUND");
-    }
-}

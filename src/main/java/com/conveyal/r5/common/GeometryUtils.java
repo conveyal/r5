362e127f6a5778dc133e6e891f6ac487d844ec1f
ansons <ansonstewart@gmail.com>
2019-06-18 23:38:10 -0400

    feat(points): reintroduce freeform pointsets
    removed in 0c27f7e5dc2cc7ca6013135602439ca3c458f88c

--- src/main/java/com/conveyal/r5/common/GeometryUtils.java
+++ src/main/java/com/conveyal/r5/common/GeometryUtils.java
@@ -1,8 +1,17 @@
 package com.conveyal.r5.common;
 
+import com.conveyal.r5.analyst.error.UnsupportedGeometryException;
 import com.conveyal.r5.streets.VertexStore;
-import com.vividsolutions.jts.geom.*;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Envelope;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineSegment;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.LinearRing;
+import com.vividsolutions.jts.geom.Polygon;
 import org.apache.commons.math3.util.FastMath;
+import org.geojson.GeoJsonObject;
 import org.geojson.LngLatAlt;
 
 import java.util.List;
@@ -81,6 +90,62 @@ public class GeometryUtils {
             throw new AssertionError("Buffer distance in geographic units is negative!");
         }
         envelope.expandBy(xExpansion, yExpansion);
+    }
+
+    /**
+     * Convert a org.geojson.Xxxx geometry to a JTS geometry. Copied from same-named class in OTP.
+     * Only support Point, Polygon and MultiPolygon for now.
+     * @param geoJsonGeom
+     * @return The equivalent JTS geometry.
+     * @throws UnsupportedGeometryException
+     * @author laurentg
+     */
+    public static Geometry convertGeoJsonToJtsGeometry(GeoJsonObject geoJsonGeom)
+            throws UnsupportedGeometryException {
+        if (geoJsonGeom instanceof org.geojson.Point) {
+            org.geojson.Point geoJsonPoint = (org.geojson.Point) geoJsonGeom;
+            return geometryFactory.createPoint(new Coordinate(geoJsonPoint.getCoordinates().getLongitude(), geoJsonPoint
+                    .getCoordinates().getLatitude()));
+
+        } else if (geoJsonGeom instanceof org.geojson.Polygon) {
+            org.geojson.Polygon geoJsonPolygon = (org.geojson.Polygon) geoJsonGeom;
+            LinearRing shell = geometryFactory.createLinearRing(convertPath(geoJsonPolygon.getExteriorRing()));
+            LinearRing[] holes = new LinearRing[geoJsonPolygon.getInteriorRings().size()];
+            int i = 0;
+            for (List<LngLatAlt> hole : geoJsonPolygon.getInteriorRings()) {
+                holes[i++] = geometryFactory.createLinearRing(convertPath(hole));
+            }
+            return geometryFactory.createPolygon(shell, holes);
+
+        } else if (geoJsonGeom instanceof org.geojson.MultiPolygon) {
+            org.geojson.MultiPolygon geoJsonMultiPolygon = (org.geojson.MultiPolygon) geoJsonGeom;
+            Polygon[] jtsPolygons = new Polygon[geoJsonMultiPolygon.getCoordinates().size()];
+            int i = 0;
+            for (List<List<LngLatAlt>> geoJsonRings : geoJsonMultiPolygon.getCoordinates()) {
+                org.geojson.Polygon geoJsonPoly = new org.geojson.Polygon();
+                for (List<LngLatAlt> geoJsonRing : geoJsonRings)
+                    geoJsonPoly.add(geoJsonRing);
+                jtsPolygons[i++] = (Polygon) convertGeoJsonToJtsGeometry(geoJsonPoly);
+            }
+            return geometryFactory.createMultiPolygon(jtsPolygons);
+
+        } else if (geoJsonGeom instanceof org.geojson.LineString) {
+            org.geojson.LineString geoJsonLineString = (org.geojson.LineString) geoJsonGeom;
+            return geometryFactory.createLineString(convertPath(geoJsonLineString.getCoordinates()));
+
+        } else if (geoJsonGeom instanceof org.geojson.MultiLineString) {
+            org.geojson.MultiLineString geoJsonMultiLineString = (org.geojson.MultiLineString) geoJsonGeom;
+            LineString[] jtsLineStrings = new LineString[geoJsonMultiLineString.getCoordinates().size()];
+            int i = 0;
+            for (List<LngLatAlt> geoJsonPath : geoJsonMultiLineString.getCoordinates()) {
+                org.geojson.LineString geoJsonLineString = new org.geojson.LineString(
+                        geoJsonPath.toArray(new LngLatAlt[geoJsonPath.size()]));
+                jtsLineStrings[i++] = (LineString) convertGeoJsonToJtsGeometry(geoJsonLineString);
+            }
+            return geometryFactory.createMultiLineString(jtsLineStrings);
+        }
+
+        throw new UnsupportedGeometryException(geoJsonGeom.getClass().toString());
     }
 
 }

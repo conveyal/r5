135f8092803d0f6a1b1790c93c051a0b2285df5f
Andrew Byrd <abyrd@conveyal.com>
2020-01-23 18:15:59 +0800

    test(getPointsInEnvelope): refactor to test characteristics of envelopes

--- src/test/java/com/conveyal/r5/analyst/WebMercatorGridPointSetTest.java
+++ src/test/java/com/conveyal/r5/analyst/WebMercatorGridPointSetTest.java
@@ -8,6 +8,9 @@ import org.locationtech.jts.geom.Envelope;
 import java.util.Arrays;
 import java.util.List;
 
+import static com.conveyal.r5.analyst.WebMercatorGridPointSetTest.TestEnvelope.Category.INSIDE;
+import static com.conveyal.r5.analyst.WebMercatorGridPointSetTest.TestEnvelope.Category.OUTSIDE;
+import static com.conveyal.r5.analyst.WebMercatorGridPointSetTest.TestEnvelope.Category.PARTIAL;
 import static com.conveyal.r5.common.GeometryUtils.floatingWgsEnvelopeToFixed;
 
 /**
@@ -40,72 +43,76 @@ public class WebMercatorGridPointSetTest extends TestCase {
         Envelope gridEnvelope = new Envelope(10, 15, -45, -44);
         WebMercatorGridPointSet gridPointSet = new WebMercatorGridPointSet(gridEnvelope);
 
-        // Entirely outside the grid, to the north of it.
-        Envelope outsideNorth = new Envelope(12, 13, -43.5, -43.4);
-
-        // Entirely outside the grid, to the south of it.
-        Envelope outsideSouth = new Envelope(13, 14, -46, -45.9);
-
-        // Entirely outside the grid, to the east of it.
-        Envelope outsideEast = new Envelope(15.9, 16, -45, -44);
-
-        // Entirely outside the grid, to the west of it.
-        Envelope outsideWest = new Envelope(9, 9.1, -45, -44);
-
-        // Entirely inside the grid, on the west side
-        Envelope insideWest = new Envelope(11, 12, -44.4, -44.6);
-
-        // Entirely inside the grid, on the east side
-        Envelope insideEast = new Envelope(13, 14, -44.5, -44.7);
-
-        // Partially overlapping the grid on the north edge
-        Envelope partialNorth = new Envelope(12, 12.5, -44.1, -43.9);
-
-        // Partially overlapping the grid on the south edge
-        Envelope partialSouth = new Envelope(13, 13.5, -45.1, -44.9);
-
-        // Partially overlapping the grid on the east edge
-        Envelope partialEast = new Envelope(14.9, 15.1, -44.4, -44.6);
-
-        // Partially overlapping the grid on the west edge
-        Envelope partialWest = new Envelope(9.9, 10.1, -44.2, -44.3);
-
-        List<Envelope> envelopes = Arrays.asList(
-                outsideNorth, outsideSouth, outsideEast, outsideWest,
-                insideEast, insideWest,
-                partialNorth, partialSouth, partialEast, partialWest
+        List<TestEnvelope> envelopes = Arrays.asList(
+            new TestEnvelope("Entirely outside the grid, to the north of it.", OUTSIDE,
+                12, 13, -43.5, -43.4),
+            new TestEnvelope("Entirely outside the grid, to the south of it.", OUTSIDE,
+                13, 14, -46, -45.9),
+            new TestEnvelope("Entirely outside the grid, to the east of it.", OUTSIDE,
+                15.9, 16, -45, -44),
+            new TestEnvelope("Entirely outside the grid, to the west of it.", OUTSIDE,
+                9, 9.1, -45, -44),
+            new TestEnvelope("Entirely inside the grid, on the west side.", INSIDE,
+                11, 12, -44.4, -44.6),
+            new TestEnvelope("Entirely inside the grid, on the east side.", INSIDE,
+                13, 14, -44.5, -44.7),
+            new TestEnvelope("Partially overlapping the grid, on the north edge.", PARTIAL,
+                12, 12.5, -44.1, -43.9),
+            new TestEnvelope("Partially overlapping the grid, on the south edge.", PARTIAL,
+                13, 13.5, -45.1, -44.9),
+            new TestEnvelope("Partially overlapping the grid, on the east edge.", PARTIAL,
+                14.9, 15.1, -44.4, -44.6),
+            new TestEnvelope("Partially overlapping the grid, on the west edge.", PARTIAL,
+                9.9, 10.1, -44.2, -44.3)
         );
-
-        // Track the number of test envelopes in each category to ensure the test is correctly formulated.
-        int outsideCount = 0;
-        int insideCount = 0;
-        int partialCount = 0;
-
-        for (Envelope envelope : envelopes) {
+        // Check that expected quantities of points are found depending on the category.
+        // Also check that our envelopes actually had the intended characteristics (according to JTS predicates).
+        for (TestEnvelope testEnvelope : envelopes) {
+            Envelope envelope = testEnvelope.envelope;
             TIntList points = gridPointSet.getPointsInEnvelope(floatingWgsEnvelopeToFixed(envelope));
-            if (gridEnvelope.contains(envelope)) {
-                insideCount += 1;
+            if (testEnvelope.category == OUTSIDE) {
+                assertFalse(gridEnvelope.intersects(envelope));
+                assertTrue(points.isEmpty());
+            } else if (testEnvelope.category == INSIDE) {
+                assertTrue(gridEnvelope.contains(envelope));
                 assertTrue(points.size() > 30_000);
-            } else if (gridEnvelope.intersects(envelope)) {
-                partialCount += 1;
+            } else if (testEnvelope.category == PARTIAL) {
+                assertTrue(gridEnvelope.intersects(envelope));
+                assertFalse(gridEnvelope.contains(envelope));
                 assertTrue(points.size() > 1_000);
                 assertTrue(points.size() < 10_000);
             } else {
-                outsideCount += 1;
-                assertTrue(points.isEmpty());
+                throw new AssertionError("Unknown category: " + testEnvelope.category);
             }
             // Every reported point index should fall within the total size of the grid.
             points.forEach(p -> {
-               assertTrue(p >= 0);
-               assertTrue(p < gridPointSet.featureCount());
-               return true;
+                assertTrue(p >= 0);
+                assertTrue(p < gridPointSet.featureCount());
+                return true;
             });
         }
+    }
 
-        // Check that our envelopes actually had the intended characteristics (according to JTS predicates).
-        assertEquals(outsideCount, 4);
-        assertEquals(insideCount, 2);
-        assertEquals(partialCount, 4);
+    public static class TestEnvelope {
+        final String description;
+        final Category category;
+        final Envelope envelope;
+
+        public TestEnvelope (
+                String description,
+                Category category,
+                double minLon,
+                double maxLon,
+                double minLat,
+                double maxLat
+        ) {
+            this.description = description;
+            this.category = category;
+            this.envelope = new Envelope(minLon, maxLon, minLat, maxLat);
+        }
+        public enum Category {
+            INSIDE, OUTSIDE, PARTIAL
+        }
     }
 
 }

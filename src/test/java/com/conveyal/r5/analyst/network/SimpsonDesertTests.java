03af2441d7dd41c769f72db27e16612ad96cd1fe
Andrew Byrd <abyrd@conveyal.com>
2021-01-22 18:46:02 +0800

    rename test class
    This avoids confusion with Grid class
    Also added some Javadoc

--- src/test/java/com/conveyal/r5/analyst/network/SimpsonDesertTests.java
+++ src/test/java/com/conveyal/r5/analyst/network/SimpsonDesertTests.java
@@ -14,12 +14,15 @@ import org.locationtech.jts.geom.CoordinateXY;
 import java.io.FileOutputStream;
 
 /**
- * FIXME rename this class and add Javadoc
+ * This is a collection of tests using roads and transit lines laid out in a large perfect grid in the desert.
+ * These networks have very predictable travel times, so instead of just checking that results don't change from one
+ * version to the next of R5 (a form of snapshot testing) this checks that they match theoretically expected travel
+ * times given headways, transfer points, distances, common trunks and competing lines, etc.
  *
- * TODO option to align grid exactly with sample points in WebMercatorGridPointSet to eliminate walking time
- *      and check splitting
+ * TODO Option to align street grid exactly with sample points in WebMercatorGridPointSet to eliminate walking time
+ *      between origin and destination and transit stops or street intersections. Also check splitting.
  */
-public class GridTest {
+public class SimpsonDesertTests {
 
     public static final Coordinate SIMPSON_DESERT_CORNER = new CoordinateXY(136.5, -25.5);
 

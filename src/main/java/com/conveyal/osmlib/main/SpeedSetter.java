36340ab524206c24ded0853633a40b457c83d9db
ansons <ansonstewart@gmail.com>
2021-04-14 15:19:52 -0400

    read speed extracts

--- src/main/java/com/conveyal/osmlib/main/SpeedSetter.java
+++ src/main/java/com/conveyal/osmlib/main/SpeedSetter.java
@@ -15,27 +15,37 @@ public class SpeedSetter {
 
     public static void main (String[] args) throws Exception {
         OSM osm = new OSM(null);
-        osm.readFromFile("/Users/abyrd/predicted_speeds_2015_for_conveyal.pbf");
+        osm.readFromFile("C:/Users/Anson/Dropbox (Conveyal)/Anson/Client " +
+                "Work/MassDOT/Streetlight/streetlight-ma-2019-02-04/massachusetts-200101.osm.pbf");
 
         System.out.println("Setting maxspeed:motorcar tags...");
-        File speedsFile = new File("/Users/abyrd/predicted_speeds_2015_for_conveyal.csv");
+        File speedsFile = new File("C:/Users/Anson/Dropbox (Conveyal)/Anson/Client " +
+                "Work/MassDOT/Streetlight/streetlight-ma-2019-02-04/speed-extract-2019.csv");
         try(BufferedReader br = new BufferedReader(new FileReader(speedsFile))) {
             br.readLine(); // skip headers
             for(String line; (line = br.readLine()) != null; ) {
                 String[] fields = line.split(",");
                 long osmWayId = Long.parseLong(fields[0]);
-                double speedKph = Double.parseDouble(fields[1]);
-                double walkFactor = Double.parseDouble(fields[2]);
-                double bikeFactor = Double.parseDouble(fields[2]);
+                double speedMph = Double.parseDouble(fields[1]);
+                if (fields.length > 10 && !fields[10].isEmpty()) {
+                    speedMph = Double.parseDouble(fields[10]);
+                }
+                // double walkFactor = Double.parseDouble(fields[2]);
+                // double bikeFactor = Double.parseDouble(fields[2]);
                 Way way = osm.ways.get(osmWayId);
-                // R5 currently prioritizes maxspeed:motorcar above all other maxspeed tags
-                way.addOrReplaceTag("maxspeed:motorcar", String.format("%1.1f kph", speedKph));
-                way.addOrReplaceTag("walk_factor", String.format("%1.1f", walkFactor));
-                way.addOrReplaceTag("bike_factor", String.format("%1.1f", bikeFactor));
-                osm.ways.put(osmWayId, way);
+                if (way == null) {
+                    System.out.println("Missing " + osmWayId);
+                } else {
+                    // R5 currently prioritizes maxspeed:motorcar above all other maxspeed tags
+                    way.addOrReplaceTag("maxspeed:motorcar", String.format("%1.1f mph", speedMph));
+                    // way.addOrReplaceTag("walk_factor", String.format("%1.1f", walkFactor));
+                    // way.addOrReplaceTag("bike_factor", String.format("%1.1f", bikeFactor));
+                    osm.ways.put(osmWayId, way);
+                }
             }
         }
-        osm.writeToFile("/Users/abyrd/predicted_speeds_output.pbf");
+        osm.writeToFile("C:/Users/Anson/Dropbox (Conveyal)/Anson/Client " +
+                "Work/MassDOT/Streetlight/streetlight-ma-2019-02-04/output.pbf");
     }
 
 }

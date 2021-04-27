6f9d36a79eaa3b261ac945e5daa17ad17b68fe02
Trevor Gerhardt <trevorgerhardt@gmail.com>
2021-04-27 18:39:29 +0300

    Fix: use getPrioritty

--- src/main/java/com/conveyal/analysis/models/Bundle.java
+++ src/main/java/com/conveyal/analysis/models/Bundle.java
@@ -82,7 +82,7 @@ public class Bundle extends Model implements Cloneable {
         public Priority priority;
         public GtfsErrorTypeSummary () { /* For deserialization. */ }
         public GtfsErrorTypeSummary (GTFSError error) {
-            this.priority = error.priority;
+            this.priority = error.getPriority();
             this.type = error.errorType;
         }
     }

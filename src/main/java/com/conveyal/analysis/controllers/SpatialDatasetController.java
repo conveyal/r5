b446ceb4a087a719896f8d5499df05f595afec75
ansons <ansonstewart@gmail.com>
2021-06-28 23:27:49 -0400

    refactor(spatial): remove unused methods
    (copied from OpportunityDatasetController for development)

--- src/main/java/com/conveyal/analysis/controllers/SpatialDatasetController.java
+++ src/main/java/com/conveyal/analysis/controllers/SpatialDatasetController.java
@@ -61,11 +61,9 @@ import static com.mongodb.client.model.Filters.eq;
 public class SpatialDatasetController implements HttpController {
 
     private static final Logger LOG = LoggerFactory.getLogger(SpatialDatasetController.class);
-
     private static final FileItemFactory fileItemFactory = new DiskFileItemFactory();
 
     // Component Dependencies
-
     private final FileStorage fileStorage;
     private final AnalysisCollection<SpatialDatasetSource> spatialSourceCollection;
     private final TaskScheduler taskScheduler;
@@ -83,13 +81,6 @@ public class SpatialDatasetController implements HttpController {
         this.extractor = extractor;
     }
 
-    private JSONObject getJSONURL (FileStorageKey key) {
-        JSONObject json = new JSONObject();
-        String url = fileStorage.getURL(key);
-        json.put("url", url);
-        return json;
-    }
-
     private List<SpatialDatasetSource> getRegionDatasets(Request req, Response res) {
         return spatialSourceCollection.findPermitted(
                 and(eq("regionId", req.params("regionId"))),
@@ -104,13 +95,7 @@ public class SpatialDatasetController implements HttpController {
     private SpatialDatasetSource downloadLODES(Request req, Response res) {
         final String regionId = req.params("regionId");
         final int zoom = parseZoom(req.queryParams("zoom"));
-
         UserPermissions userPermissions = req.attribute(USER_PERMISSIONS_ATTRIBUTE);
-        final Region region = Persistence.regions.findByIdIfPermitted(regionId, userPermissions.accessGroup);
-        // Common UUID for all LODES datasets created in this download (e.g. so they can be grouped together and
-        // deleted as a batch using deleteSourceSet)
-        // The bucket name contains the specific lodes data set and year so works as an appropriate name
-
         SpatialDatasetSource source = SpatialDatasetSource.create(userPermissions, extractor.sourceName)
                 .withRegion(regionId);
 
@@ -123,125 +108,6 @@ public class SpatialDatasetController implements HttpController {
                 }));
 
         return source;
-    }
-
-    /**
-     * Given a list of new PointSets, serialize each PointSet and save it to S3, then create a metadata object about
-     * that PointSet and store it in Mongo.
-     */
-    private void updateAndStoreDatasets (SpatialDatasetSource source,
-                                         List<? extends PointSet> pointSets) {
-
-        // Create an OpportunityDataset holding some metadata about each PointSet (Grid or FreeForm).
-        final List<OpportunityDataset> datasets = new ArrayList<>();
-        for (PointSet pointSet : pointSets) {
-            OpportunityDataset dataset = new OpportunityDataset();
-            dataset.sourceName = source.name;
-            dataset.sourceId = source._id.toString();
-            dataset.createdBy = source.createdBy;
-            dataset.accessGroup = source.accessGroup;
-            dataset.regionId = source.regionId;
-            dataset.name = pointSet.name;
-            dataset.totalPoints = pointSet.featureCount();
-            dataset.totalOpportunities = pointSet.sumTotalOpportunities();
-            dataset.format = getFormatCode(pointSet);
-            if (dataset.format == FileStorageFormat.FREEFORM) {
-                dataset.name = String.join(" ", pointSet.name, "(freeform)");
-            }
-            dataset.setWebMercatorExtents(pointSet);
-            // TODO make origin and destination pointsets reference each other and indicate they are suitable
-            //      for one-to-one analyses
-
-            // Store the PointSet metadata in Mongo and accumulate these objects into the method return list.
-            Persistence.opportunityDatasets.create(dataset);
-            datasets.add(dataset);
-
-            // Persist a serialized representation of each PointSet (not the metadata) to S3 or other object storage.
-            // TODO this should probably be pulled out to another method, and possibly called one frame up.
-            //      Persisting the PointSets to S3 is a separate task than making metadata and storing in Mongo.
-            try {
-                if (pointSet instanceof Grid) {
-                    File gridFile = FileUtils.createScratchFile("grid");
-
-                    OutputStream fos = new GZIPOutputStream(new FileOutputStream(gridFile));
-                    ((Grid)pointSet).write(fos);
-
-                    fileStorage.moveIntoStorage(dataset.getStorageKey(FileStorageFormat.GRID), gridFile);
-                } else if (pointSet instanceof FreeFormPointSet) {
-                    // Upload serialized freeform pointset back to S3
-                    FileStorageKey fileStorageKey = new FileStorageKey(GRIDS, source.regionId + "/" + dataset._id +
-                            ".pointset");
-                    File pointsetFile = FileUtils.createScratchFile("pointset");
-
-                    OutputStream os = new GZIPOutputStream(new FileOutputStream(pointsetFile));
-                    ((FreeFormPointSet)pointSet).write(os);
-
-                    fileStorage.moveIntoStorage(fileStorageKey, pointsetFile);
-                } else {
-                    throw new IllegalArgumentException("Unrecognized PointSet type, cannot persist it.");
-                }
-                // TODO task tracking
-            } catch (NumberFormatException e) {
-                throw new AnalysisServerException("Error attempting to parse number in uploaded file: " + e.toString());
-            } catch (Exception e) {
-                throw AnalysisServerException.unknown(e);
-            }
-        }
-    }
-
-    private static FileStorageFormat getFormatCode (PointSet pointSet) {
-        if (pointSet instanceof FreeFormPointSet) {
-            return FileStorageFormat.FREEFORM;
-        } else if (pointSet instanceof Grid) {
-            return FileStorageFormat.GRID;
-        } else {
-            throw new RuntimeException("Unknown pointset type.");
-        }
-    }
-
-    /**
-     * Given a CSV file, converts each property (CSV column) into a freeform (non-gridded) pointset.
-     *
-     * The provided multipart form data must include latField and lonField. To indicate paired origins and destinations
-     * (e.g. to use results from an origin-destination survey in a one-to-one regional analysis), the form data should
-     * include the optional latField2 and lonField2 fields.
-     *
-     * This method executes in a blocking (synchronous) manner, but it can take a while so should be called within an
-     * non-blocking asynchronous task.
-     */
-    private List<FreeFormPointSet> createFreeFormPointSetsFromCsv(FileItem csvFileItem, Map<String, String> params) {
-
-        String latField = params.get("latField");
-        String lonField = params.get("lonField");
-        if (latField == null || lonField == null) {
-            throw AnalysisServerException.fileUpload("You must specify a latitude and longitude column.");
-        }
-
-        // The name of the column containing a unique identifier for each row. May be missing (null).
-        String idField = params.get("idField");
-
-        // The name of the column containing the opportunity counts at each point. May be missing (null).
-        String countField = params.get("countField");
-
-        // Optional secondary latitude, longitude, and count fields.
-        // This allows you to create two matched parallel pointsets of the same size with the same IDs.
-        String latField2 = params.get("latField2");
-        String lonField2 = params.get("lonField2");
-
-        try {
-            List<FreeFormPointSet> pointSets = new ArrayList<>();
-            InputStreamProvider csvStreamProvider = new FileItemInputStreamProvider(csvFileItem);
-            pointSets.add(FreeFormPointSet.fromCsv(csvStreamProvider, latField, lonField, idField, countField));
-            // The second pair of lat and lon fields allow creating two matched pointsets from the same CSV.
-            // This is used for one-to-one travel times between specific origins/destinations.
-            if (latField2 != null && lonField2 != null) {
-                pointSets.add(FreeFormPointSet.fromCsv(csvStreamProvider, latField2, lonField2, idField, countField));
-            }
-            return pointSets;
-        } catch (Exception e) {
-            throw AnalysisServerException.fileUpload("Could not convert CSV to Freeform PointSet: " + e.toString());
-        }
-
     }
 
     /**
@@ -323,10 +189,6 @@ public class SpatialDatasetController implements HttpController {
         return source;
     }
 
-    private OpportunityDataset editOpportunityDataset(Request request, Response response) throws IOException {
-        return Persistence.opportunityDatasets.updateFromJSONRequest(request);
-    }
-
     private Collection<SpatialDatasetSource> deleteSourceSet(Request request, Response response) {
         SpatialDatasetSource source = spatialSourceCollection.findPermittedByRequestParamId(request, response);
         // TODO delete files from storage
@@ -339,88 +201,6 @@ public class SpatialDatasetController implements HttpController {
         );
     }
 
-    private OpportunityDataset deleteOpportunityDataset(Request request, Response response) {
-        String opportunityDatasetId = request.params("_id");
-        return deleteDataset(opportunityDatasetId, request.attribute("accessGroup"));
-    }
-
-    /**
-     * Delete an Opportunity Dataset from the database and all formats from the file store.
-     */
-    private OpportunityDataset deleteDataset(String id, String accessGroup) {
-        OpportunityDataset dataset = Persistence.opportunityDatasets.removeIfPermitted(id, accessGroup);
-
-        if (dataset == null) {
-            throw AnalysisServerException.notFound("Opportunity dataset could not be found.");
-        } else {
-            fileStorage.delete(dataset.getStorageKey(FileStorageFormat.GRID));
-            fileStorage.delete(dataset.getStorageKey(FileStorageFormat.PNG));
-            fileStorage.delete(dataset.getStorageKey(FileStorageFormat.TIFF));
-        }
-
-        return dataset;
-    }
-
-    /**
-     * Create a grid from WGS 84 points in a CSV file.
-     * The supplied CSV file will not be deleted - it may be used again to make another (freeform) pointset.
-     * TODO explain latField2 usage
-     * @return one or two Grids for each numeric column in the CSV input.
-     */
-    private List<Grid> createGridsFromCsv(FileItem csvFileItem,
-                                                 Map<String, List<FileItem>> query,
-                                                 int zoom) throws Exception {
-
-        String latField = getFormField(query, "latField", true);
-        String lonField = getFormField(query, "lonField", true);
-        String idField = getFormField(query, "idField", false);
-
-        // Optional fields to run grid construction twice with two different sets of points.
-        // This is only really useful when creating grids to visualize freeform pointsets for one-to-one analyses.
-        String latField2 = getFormField(query, "latField2", false);
-        String lonField2 = getFormField(query, "lonField2", false);
-
-        List<String> ignoreFields = Arrays.asList(idField, latField2, lonField2);
-        InputStreamProvider csvStreamProvider = new FileItemInputStreamProvider(csvFileItem);
-        List<Grid> grids = Grid.fromCsv(csvStreamProvider, latField, lonField, ignoreFields, zoom, null);
-        // TODO verify correctness of this second pass
-        if (latField2 != null && lonField2 != null) {
-            ignoreFields = Arrays.asList(idField, latField, lonField);
-            grids.addAll(Grid.fromCsv(csvStreamProvider, latField2, lonField2, ignoreFields, zoom, null));
-        }
-
-        return grids;
-    }
-
-    /**
-     * Create a grid from an input stream containing a binary grid file.
-     * For those in the know, we can upload manually created binary grid files.
-     */
-    private List<Grid> createGridsFromBinaryGridFiles(List<FileItem> uploadedFiles) throws Exception {
-
-        List<Grid> grids = new ArrayList<>();
-        // TODO task size with uploadedFiles.size();
-        for (FileItem fileItem : uploadedFiles) {
-            Grid grid = Grid.read(fileItem.getInputStream());
-            String name = fileItem.getName();
-            // Remove ".grid" from the name
-            if (name.contains(".grid")) name = name.split(".grid")[0];
-            grid.name = name;
-            // TODO task progress
-            grids.add(grid);
-        }
-        // TODO mark task complete
-        return grids;
-    }
-
-    /**
-     * Preconditions: fileItems must contain SHP, DBF, and PRJ files, and optionally SHX. All files should have the
-     * same base name, and should not contain any other files but these three or four.
-     */
-    private void createGridsFromShapefile(List<FileItem> fileItems) throws Exception {
-        // TODO implement rasterization methods
-    }
-
     @Override
     public void registerEndpoints (spark.Service sparkService) {
         sparkService.path("/api/spatial", () -> {
@@ -428,9 +208,7 @@ public class SpatialDatasetController implements HttpController {
             sparkService.post("/region/:regionId/download", this::downloadLODES, toJson);
             sparkService.get("/region/:regionId", this::getRegionDatasets, toJson);
             sparkService.delete("/source/:_id", this::deleteSourceSet, toJson);
-            sparkService.delete("/:_id", this::deleteOpportunityDataset, toJson);
             sparkService.get("/:_id", this::getSource, toJson);
-            sparkService.put("/:_id", this::editOpportunityDataset, toJson);
         });
     }
 }

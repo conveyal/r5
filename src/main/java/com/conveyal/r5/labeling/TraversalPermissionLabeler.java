8455e09c3d8cdf4e5fd1a502c2a027bd97785270
ansons <ansonstewart@gmail.com>
2021-07-07 15:42:20 -0400

    feat(osm): allow traversal of driveways

--- src/main/java/com/conveyal/r5/labeling/TraversalPermissionLabeler.java
+++ src/main/java/com/conveyal/r5/labeling/TraversalPermissionLabeler.java
@@ -56,7 +56,7 @@ public abstract class TraversalPermissionLabeler {
         validHighwayTags = Collections.unmodifiableMap(validHighwayTagsConst);
 
         addPermissions("motorway", "access=yes;bicycle=no;foot=no");
-        addPermissions("trunk|primary|secondary|tertiary|unclassified|residential|living_street|road|service|track", "access=yes");
+        addPermissions("trunk|primary|secondary|tertiary|unclassified|residential|living_street|road|service|track|driveway", "access=yes");
         addPermissions("pedestrian", "access=no;foot=yes");
         addPermissions("path", "access=no;foot=yes;bicycle=yes");
         addPermissions("bridleway", "access=no"); //horse=yes but we don't support horse

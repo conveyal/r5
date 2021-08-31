049e92f82ef49011f12bea5597b0b3f01226866f
Andrew Byrd <abyrd@conveyal.com>
2021-08-31 17:49:16 +0800

    assert ERROR issues when geometries are mismatched

--- src/test/java/com/conveyal/analysis/datasource/GeoJsonDataSourceIngesterTest.java
+++ src/test/java/com/conveyal/analysis/datasource/GeoJsonDataSourceIngesterTest.java
@@ -1,12 +1,15 @@
 package com.conveyal.analysis.datasource;
 
+import com.conveyal.analysis.models.DataSourceValidationIssue;
 import com.conveyal.analysis.models.SpatialDataSource;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
 import static com.conveyal.analysis.datasource.SpatialDataSourceIngesterTest.ingest;
+import static com.conveyal.analysis.models.DataSourceValidationIssue.Level.ERROR;
 import static com.conveyal.file.FileStorageFormat.GEOJSON;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -19,21 +22,32 @@ class GeoJsonDataSourceIngesterTest {
     @Test
     void typeMismatch () {
         SpatialDataSource spatialDataSource = ingest(GEOJSON, "hkzones-type-mismatch");
+        // Currently we can't detect problems with inconsistent schema across features.
+        // GeoTools seems to report the same schema on every feature.
+        assertTrue(spatialDataSource.issues.isEmpty());
     }
 
     @Test
     void extraAttribute () {
         SpatialDataSource spatialDataSource = ingest(GEOJSON, "hkzones-extra-attribute");
+        // Currently we can't detect problems with inconsistent schema across features.
+        // GeoTools seems to report the same schema on every feature.
+        assertTrue(spatialDataSource.issues.isEmpty());
     }
 
     @Test
     void mixedNumeric () {
         SpatialDataSource spatialDataSource = ingest(GEOJSON, "hkzones-mixed-numeric");
+        // Currently we can't detect problems with inconsistent schema across features.
+        // GeoTools seems to report the same schema on every feature.
+        assertTrue(spatialDataSource.issues.isEmpty());
     }
 
     @Test
     void mixedGeometries () {
         SpatialDataSource spatialDataSource = ingest(GEOJSON, "hkzones-mixed-geometries");
+        // Inconsistent geometry between features is detected.
+        assertTrue(spatialDataSource.issues.stream().anyMatch(i -> i.level == ERROR));
     }
 
     @Test

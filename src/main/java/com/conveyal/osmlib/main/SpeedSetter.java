e8228c9ddaa6d882046bcc2c64e9c28ca134f709
ansons <ansonstewart@gmail.com>
2021-09-03 10:22:33 -0400

    feat(speeds): set speeds for different time periods

--- src/main/java/com/conveyal/osmlib/main/SpeedSetter.java
+++ src/main/java/com/conveyal/osmlib/main/SpeedSetter.java
@@ -10,7 +10,9 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -24,86 +26,94 @@ public class SpeedSetter {
     static final String osmInputFile = dir + "massachusetts-200101-cropped.osm.pbf";
     static final String speedInputFile = dir + "speed-extract-2019_v5.csv";
     static final String speedFallbackFile = dir + "fallbacks.csv";
-    static final String osmOutputFile = dir + "2019-12pm-3pm_5.2.pbf";
-    static final String csvOutputFile = dir + "speeds-12pm-3pm_v5.2.csv";
+    static final String osmOutputFile = dir + "2019_5.4";
+    static final String csvOutputFile = dir + "speeds_v5.4";
+    static final int timePeriods = 8;
 
     // Column numbers in supplied CSVs
-
-    // For 6-9am
-    // static final int speedIndex = 3;
-    // static final int fallbackIndex = 5;
-
-    // For 12-3pm
-    static final int speedIndex = 5;
-    static final int fallbackIndex = 7;
+    static final int speedIndexOffset = 1;
+    static final int fallbackIndexOffset = 3;
 
     public static void main (String[] args) throws Exception {
         OSM osm = new OSM(null);
         osm.readFromFile(osmInputFile);
 
+        // Prepare maps to store speed and fallback speed values
+        List<Map<Long, Double>> speeds = new ArrayList<>();
+        List<Map<String, Double>> fallbacks = new ArrayList<>();
+        for (int timePeriod = 0; timePeriod < timePeriods; timePeriod++) {
+            speeds.add(new HashMap<>());
+            fallbacks.add(new HashMap<>());
+        }
+
         // Populate map from OSM way IDs to speeds using provided CSV
-        Map<Long, Double> speeds = new HashMap<>();
-        File speedsFile = new File(speedInputFile);
-        BufferedReader br = new BufferedReader(new FileReader(speedsFile));
-        br.readLine(); // skip headers
-        for (String line; (line = br.readLine()) != null; ) {
+        BufferedReader speedReader = new BufferedReader(new FileReader(speedInputFile));
+        speedReader.readLine(); // skip headers
+        for (String line; (line = speedReader.readLine()) != null; ) {
             String[] fields = line.split(",");
             long osmWayId = Long.parseLong(fields[0]);
-            double speedMph = Double.parseDouble(fields[speedIndex]);
-            speeds.put(osmWayId, speedMph);
+            for (int timePeriod = 0; timePeriod < timePeriods; timePeriod++) {
+                speeds.get(timePeriod).put(osmWayId, Double.parseDouble(fields[speedIndexOffset + timePeriod]));
+            }
         }
 
         // Populate map from OSM highway tags to fallback speed values using provided CSV
-        Map<String, Double> fallbacks = new HashMap<>();
-        br = new BufferedReader(new FileReader(speedFallbackFile));
-        br.readLine(); // skip headers
-        for (String line; (line = br.readLine()) != null; ) {
+        BufferedReader fallbackReader = new BufferedReader(new FileReader(speedFallbackFile));
+        fallbackReader.readLine(); // skip headers
+        for (String line; (line = fallbackReader.readLine()) != null; ) {
             String[] fields = line.split(",");
             String osmHighwayTag = fields[0];
-            double speedMph = Double.parseDouble(fields[fallbackIndex]);
-            fallbacks.put(osmHighwayTag, speedMph);
+            for (int timePeriod = 0; timePeriod < timePeriods; timePeriod++) {
+                fallbacks.get(timePeriod).put(osmHighwayTag,
+                        Double.parseDouble(fields[fallbackIndexOffset + timePeriod]));
+            }
         }
 
         SpeedConfig estimatedSpeedLimits = SpeedConfig.defaultConfig();
 
         System.out.println("Setting maxspeed:motorcar tags...");
 
-        Map<Long, Way> ways = new HashMap<>();
+        for (int timePeriod = 0; timePeriod < timePeriods; timePeriod++) {
+            System.out.println("Time period " + timePeriod);
 
-        File output = new File(csvOutputFile);
+            Map<Long, Way> ways = new HashMap<>();
+            File output = new File(csvOutputFile + "_" + timePeriod + ".csv");
+            BufferedWriter bw = new BufferedWriter(new FileWriter(output));
+            bw.write(String.join(",", "osmId", "streetType", "speedMph"));
+            bw.newLine();
 
-        BufferedWriter bw = new BufferedWriter(new FileWriter(output));
-        bw.write(String.join(",","osmId", "streetType", "speedMph"));
-        bw.newLine();
+            int tp = timePeriod; // To satisfy Java lambda effectively final requirements
+            osm.ways.forEach((osmWayId, way) -> {
+                String streetType;
+                Double speedMph = null;
 
-        osm.ways.forEach((osmWayId, way )->  {
-            String streetType;
-            Double speedMph = null;
+                if (osmWayId == null || way == null) {
+                    streetType = "Missing";
+                } else {
+                    streetType = way.getTag("highway") == null ? "Untagged" : way.getTag("highway");
+                    // TODO make this configurable (add arg "enforceSpeedLimits"?)
+                    Integer speedLimit = estimatedSpeedLimits.values.getOrDefault(streetType, estimatedSpeedLimits.defaultSpeed);
+                    double fallbackSpeed = fallbacks.get(tp).get(streetType) == null ? speedLimit :
+                            fallbacks.get(tp).get(streetType);
+                    // TODO use fallback value by streetType instead of speed limit?
+                    speedMph = speeds.get(tp).get(osmWayId) == null ? fallbackSpeed : Math.min(speedLimit,
+                            speeds.get(tp).get(osmWayId));
+                    // R5 currently prioritizes maxspeed:motorcar above all other maxspeed tags
+                    way.addOrReplaceTag("maxspeed:motorcar", String.format("%1.1f mph", speedMph));
+                    ways.put(osmWayId, way);
+                }
+                try {
+                    bw.write(String.join(",", String.valueOf(osmWayId), streetType, String.valueOf(speedMph)));
+                    bw.newLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
-            if (osmWayId == null || way == null) {
-                streetType = "Missing";
-            } else {
-                streetType = way.getTag("highway") == null ? "Untagged" : way.getTag("highway");
-                // TODO make this configurable (add arg "enforceSpeedLimits"?)
-                Integer speedLimit = estimatedSpeedLimits.values.getOrDefault(streetType, estimatedSpeedLimits.defaultSpeed);
-                double fallbackSpeed = fallbacks.get(streetType) == null ? speedLimit : fallbacks.get(streetType);
-                // TODO use fallback value by streetType instead of speed limit?
-                speedMph = speeds.get(osmWayId) == null ? fallbackSpeed : Math.min(speedLimit, speeds.get(osmWayId));
-                // R5 currently prioritizes maxspeed:motorcar above all other maxspeed tags
-                way.addOrReplaceTag("maxspeed:motorcar", String.format("%1.1f mph", speedMph));
-                ways.put(osmWayId, way);
-            }
-            try {
-                bw.write(String.join(",", String.valueOf(osmWayId), streetType, String.valueOf(speedMph)));
-                bw.newLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-        });
-        osm.ways = ways;
-        osm.writeToFile(osmOutputFile);
-        bw.close();
+            });
+            osm.ways = ways;
+            osm.writeToFile(osmOutputFile + "_" + timePeriod + ".pbf");
+            bw.close();
+        }
     }
 
 }

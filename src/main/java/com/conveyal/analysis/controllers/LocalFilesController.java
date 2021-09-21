48558a42298d703950f482a559f56c1112fa75e3
Andrew Byrd <abyrd@conveyal.com>
2021-09-21 17:22:30 +0800

    use same API path as UI for aggregation grid URLs
    Also return empty string instead of null in LocalFilesController and
    document why - when Spark framework sees a null response it thinks the
    path is not mapped to a controller.

--- src/main/java/com/conveyal/analysis/controllers/LocalFilesController.java
+++ src/main/java/com/conveyal/analysis/controllers/LocalFilesController.java
@@ -5,6 +5,7 @@ import com.conveyal.file.FileStorage;
 import com.conveyal.file.FileStorageFormat;
 import com.conveyal.file.FileStorageKey;
 import com.conveyal.file.FileUtils;
+import com.conveyal.file.LocalFileStorage;
 import spark.Request;
 import spark.Response;
 import spark.Service;
@@ -20,14 +21,13 @@ import java.util.Locale;
  */
 public class LocalFilesController implements HttpController {
 
-    // Something feels whack here, this should more specifically be a LocalFileStorage
-    private final FileStorage fileStorage;
+    private final LocalFileStorage fileStorage;
 
     public LocalFilesController (FileStorage fileStorage) {
-        this.fileStorage = fileStorage;
+        this.fileStorage = (LocalFileStorage) fileStorage;
     }
 
-    private InputStream getFile (Request req, Response res) throws Exception {
+    private Object getFile (Request req, Response res) throws Exception {
         String filename = req.splat()[0];
         FileCategory category = FileCategory.valueOf(req.params("category").toUpperCase(Locale.ROOT));
         FileStorageKey key = new FileStorageKey(category, filename);
@@ -35,15 +35,21 @@ public class LocalFilesController implements HttpController {
         FileStorageFormat format = FileStorageFormat.fromFilename(filename);
         res.type(format.mimeType);
 
-        // If the content-encoding is set to gzip, Spark automatically gzips the response. This mangles data
-        // that was already gzipped. Therefore, check if it's gzipped and pipe directly to the raw OutputStream.
+        // If the content-encoding is set to gzip, Spark automatically gzips the response. This double-gzips anything
+        // that was already gzipped. Some of our files are already gzipped, and we rely on the the client browser to
+        // decompress them upon receiving them. Therefore, when serving a file that's already gzipped we bypass Spark,
+        // piping it directly to the raw Jetty OutputStream. As soon as transferFromFileTo completes it closes the
+        // output stream, which completes the HTTP response to the client. We must then return something to Spark. We
+        // can't return null because Spark will spew errors about the endpoint being "not mapped" and try to replace
+        // the response with a 404, so we return an empty String.
         res.header("Content-Encoding", "gzip");
         if (FileUtils.isGzip(file)) {
             // TODO Trace in debug: how does this actually work?
             //      Verify what this is transferring into - a buffer? In another reading thread?
             //      Is Jetty ServletOutputStream implementation automatically threaded or buffered?
+            //      It appears to be buffered because the response has a Content-Length header.
             FileUtils.transferFromFileTo(file, res.raw().getOutputStream());
-            return null;
+            return "";
         } else {
             return FileUtils.getInputStream(file);
         }

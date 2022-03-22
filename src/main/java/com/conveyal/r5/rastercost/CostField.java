56cad7a32e522e8f3f7cb8c5ebc658ba6402196d
Andrew Byrd <andrew@fastmail.net>
2022-03-23 00:38:12 +0800

    clarify javadoc

--- src/main/java/com/conveyal/r5/rastercost/CostField.java
+++ src/main/java/com/conveyal/r5/rastercost/CostField.java
@@ -15,11 +15,9 @@ import java.util.List;
 /**
  * Subclasses or plugin functions will provide costs for hills, sun, noise, pollution etc.
  *
- * Interface-wise, a CostField is like a TraversalTimeCalculator with no turn costs, but it can transform an existing
- * cost. So TraversalTimeCalculator is like a special case of CostField that is always called first with a
- * parameter of zero, and has the added ability to calculate turn costs.
- *
- * TODO rename to TraversalTimeTransformer or TraversalCost
+ * Interface-wise, a CostField is like a TraversalTimeCalculator with no turn costs, which transforms an existing
+ * traversal time rather than starting from scratch. So TraversalTimeCalculator is like a special case of CostField
+ * that is always called first with an input traversalTimeSeconds of zero, and can also calculate turn costs.
  */
 public interface CostField {
 

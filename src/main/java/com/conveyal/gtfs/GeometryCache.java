30a26c112fd6e3d50c42a452d6af394d5f129937
Andrew Byrd <abyrd@conveyal.com>
2022-03-31 19:45:35 +0800

    update javadoc

--- src/main/java/com/conveyal/gtfs/GeometryCache.java
+++ src/main/java/com/conveyal/gtfs/GeometryCache.java
@@ -14,7 +14,13 @@ import java.util.List;
 import java.util.function.BiConsumer;
 
 /**
- * Cache sets of geometry in a JTS STRtree using a Caffeine LoadingCache.
+ * Cache STRTree spatial indexes of geometries of a particular type, with each spatial index keyed on a String
+ * (typically the bundle-scoped feed ID of the GTFS feed the geometries are drawn from). This is based on a Caffeine
+ * LoadingCache so should be thread safe and provide granular per-key locking, which is convenient when serving up
+ * lots of simultaneous vector tile requests.
+ *
+ * This is currently used only for looking up geomertries when producing Mapbox vector map tiles, hence the single
+ * set of hard-wired cache eviction parameters. For more general use we'd want another constructor to change them.
  */
 public class GeometryCache<T extends Geometry> {
     private static final Logger LOG = LoggerFactory.getLogger(GeometryCache.class);
@@ -22,7 +28,7 @@ public class GeometryCache<T extends Geometry> {
     /** How long after it was last accessed to keep a spatial index in memory. */
     private static final Duration EXPIRE_AFTER_ACCESS = Duration.ofMinutes(10);
 
-    /** The maximum number of spatial indexes for which we keep in memory at once. */
+    /** The maximum number of spatial indexes to keep in memory at once. */
     private static final int MAX_SPATIAL_INDEXES = 4;
 
     /** A cache of spatial indexes, usually keyed on a feed ID. */
@@ -49,6 +55,7 @@ public class GeometryCache<T extends Geometry> {
     /**
      * Return a List including all geometries that intersect a given envelope.
      * Note that this overselects (can and will return some geometries outside the envelope).
+     * It could make sense to move some of the clipping logic in here if this class remains vector tile specific.
      */
     public List<T> queryEnvelope(String key, Envelope envelope) {
         return cache.get(key).query(envelope);

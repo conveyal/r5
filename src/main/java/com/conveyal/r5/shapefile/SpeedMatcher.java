2564cb127c62017f36d9093243d7a8b2467b4ef9
ansons <ansonstewart@gmail.com>
2022-05-23 16:05:23 -0400

    feat: add shapefile speed matcher

---
+++ src/main/java/com/conveyal/r5/shapefile/SpeedMatcher.java
@@ -0,0 +1,25 @@
+package com.conveyal.r5.shapefile;
+
+import com.conveyal.r5.streets.EdgeStore;
+import com.conveyal.r5.streets.StreetLayer;
+import org.opengis.feature.simple.SimpleFeature;
+
+public class SpeedMatcher extends ShapefileMatcher {
+
+    public SpeedMatcher (StreetLayer streets) {
+        super(streets);
+    }
+
+    /**
+     * Set the speed value of an edge using a supplied feature with a speed attribute (in kilometers per hour)
+     */
+    @Override
+    void setEdgePair (SimpleFeature feature, int attributeIndex, EdgeStore.Edge edge) {
+        if (feature.getAttribute(attributeIndex) != null) {
+            double speedKph = ((Number) feature.getAttribute(attributeIndex)).doubleValue();
+            edge.setSpeedKph(speedKph);
+            edge.advance();
+            edge.setSpeedKph(speedKph);
+        }
+    }
+}

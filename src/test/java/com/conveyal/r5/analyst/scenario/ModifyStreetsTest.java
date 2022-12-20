adfeca9d64439158ef731de2f192705782a43bd4
Andrew Byrd <andrew@fastmail.net>
2022-12-20 20:29:02 +0800

    fix iteration over found vertices use inequalities for softer assertions style changes, use Arrays.asList()

--- src/test/java/com/conveyal/r5/analyst/scenario/ModifyStreetsTest.java
+++ src/test/java/com/conveyal/r5/analyst/scenario/ModifyStreetsTest.java
@@ -1,6 +1,5 @@
 package com.conveyal.r5.analyst.scenario;
 
-import com.beust.jcommander.internal.Lists;
 import com.conveyal.r5.analyst.WebMercatorExtents;
 import com.conveyal.r5.analyst.WebMercatorGridPointSet;
 import com.conveyal.r5.analyst.cluster.TravelTimeSurfaceTask;
@@ -9,29 +8,37 @@ import com.conveyal.r5.api.util.TransitModes;
 import com.conveyal.r5.profile.StreetMode;
 import com.conveyal.r5.streets.StreetRouter;
 import com.conveyal.r5.transit.TransportNetwork;
+import com.conveyal.r5.util.LambdaCounter;
 import gnu.trove.map.TIntIntMap;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.locationtech.jts.geom.Envelope;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.EnumSet;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
- * Test that modifying streets works correctly.
+ * Test that Modifications affecting streets work correctly.
  */
 public class ModifyStreetsTest {
-    double fromLat = 40.0;
-    double fromLon = -83.0;
-    int timeLimitSeconds = 1200;
+
+    final double FROM_LAT = 40.0;
+    final double FROM_LON = -83.0;
+    final int TIME_LIMIT_SECONDS = 1200;
     // Analysis bounds
-    double SIZE = 0.05;
-    double WEST = fromLon - SIZE;
-    double EAST = fromLon + SIZE;
-    double SOUTH = fromLat - SIZE;
-    double NORTH = fromLat + SIZE;
+    final double SIZE_DEGREES = 0.05;
+    final double WEST = FROM_LON - SIZE_DEGREES;
+    final double EAST = FROM_LON + SIZE_DEGREES;
+    final double SOUTH = FROM_LAT - SIZE_DEGREES;
+    final double NORTH = FROM_LAT + SIZE_DEGREES;
 
     /**
      * Using a `ModifyStreets` modification, test that adjusting the `walkTimeFactor` appropriately changes route time.
+     * Also indirectly tests that the necessary generalized cost data tables will be added to the network when missing.
      */
     @Test
     public void testGeneralizedCostWalk() {
@@ -49,24 +56,33 @@ public class ModifyStreetsTest {
         }};
 
         var scenario = new Scenario();
-        scenario.modifications = Lists.newArrayList(ms);
+        scenario.modifications = Arrays.asList(ms);
 
-        var reachedVertexes = getReachedVertexes(network, new Scenario());
-        var reachedVertexesWithModification = getReachedVertexes(network, scenario);
+        var reachedVertices = getReachedVertices(network, new Scenario());
+        var reachedVerticesWithModification = getReachedVertices(network, scenario);
 
-        int totalGreater = 0;
-        for (int i = 0; i < reachedVertexes.size(); i++) {
-            int baselineTime = reachedVertexes.get(i);
-            int modTime = reachedVertexesWithModification.get(i);
-            Assertions.assertTrue(baselineTime >= modTime);
-            if (baselineTime > modTime) {
-                totalGreater++;
+        int nReachedVertices = reachedVertices.size();
+        int nReachedVerticesWithModification = reachedVerticesWithModification.size();
+
+        assertTrue(nReachedVertices > 500);
+        assertTrue(nReachedVerticesWithModification > nReachedVertices * 2);
+
+        int[] nLowerTimes = new int[1]; // Sidestep annoying Java "effectively final" rule.
+        reachedVertices.forEachKey(v -> {
+            int travelTime = reachedVertices.get(v);
+            int travelTimeWithModification = reachedVerticesWithModification.get(v);
+            assertTrue(travelTimeWithModification != -1);
+            assertTrue(travelTimeWithModification <= travelTime);
+            if (travelTimeWithModification != travelTime) {
+                nLowerTimes[0] += 1;
+                assertTrue(travelTimeWithModification * 1.3 < travelTime);
             }
-        }
-        Assertions.assertEquals(454, totalGreater);
+            return true;
+        });
+        assertTrue(nLowerTimes[0] > 500);
     }
 
-    TIntIntMap getReachedVertexes (TransportNetwork network, Scenario scenario) {
+    TIntIntMap getReachedVertices(TransportNetwork network, Scenario scenario) {
         var modifiedNetwork = scenario.applyToTransportNetwork(network);
         Assertions.assertEquals(0, modifiedNetwork.scenarioApplicationWarnings.size());
 
@@ -76,8 +92,8 @@ public class ModifyStreetsTest {
         task.directModes = EnumSet.of(LegMode.WALK);
         task.transitModes = EnumSet.noneOf(TransitModes.class);
         task.percentiles = new int[]{50};
-        task.fromLat = fromLat;
-        task.fromLon = fromLon;
+        task.fromLat = FROM_LAT;
+        task.fromLon = FROM_LON;
         task.north = extents.north;
         task.west = extents.west;
         task.height = extents.height;
@@ -88,7 +104,7 @@ public class ModifyStreetsTest {
         streetRouter.profileRequest = task;
         streetRouter.setOrigin(task.fromLat, task.fromLon);
         streetRouter.streetMode = StreetMode.WALK;
-        streetRouter.timeLimitSeconds = timeLimitSeconds;
+        streetRouter.timeLimitSeconds = TIME_LIMIT_SECONDS;
         streetRouter.route();
 
         return streetRouter.getReachedVertices();

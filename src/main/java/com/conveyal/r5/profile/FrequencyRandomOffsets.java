d5dbafcbc156074eda15186a4220ab599519f18b
ansons <ansonstewart@gmail.com>
2023-06-09 13:10:08 -0400

    Hardcode locked-schedules
    For testing only, until backend can be restarted with update to copy lockSchedules parameter.

--- src/main/java/com/conveyal/r5/profile/FrequencyRandomOffsets.java
+++ src/main/java/com/conveyal/r5/profile/FrequencyRandomOffsets.java
@@ -50,12 +50,8 @@ public class FrequencyRandomOffsets {
         if (!data.hasFrequencies) {
             return;
         }
-        if (request == null || !request.lockSchedules) {
-            this.mt = new MersenneTwister();
-        } else {
-            // Use a fixed seed for each origin
-            this.mt = new MersenneTwister((int) (request.fromLat * 1e9));
-        }
+        // Use a fixed seed for each origin
+        this.mt = new MersenneTwister((int) (request.fromLat * 1e9));
         // Create skeleton empty data structure with slots for all offsets that will be generated.
         for (int pattIdx = 0; pattIdx < data.tripPatterns.size(); pattIdx++) {
             TripPattern tp = data.tripPatterns.get(pattIdx);

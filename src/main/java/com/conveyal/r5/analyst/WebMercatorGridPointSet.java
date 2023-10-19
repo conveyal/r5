fad70b389b35a10b966db74dfb46a63e3b213227
Andrew Byrd <andrew@fastmail.net>
2023-10-19 21:54:43 +0200

    Compose with WebMercatorExtents to reduce duplication
    Height and width in WebMercatorGridPointSet constructor were often one
    unit too narrow (not consistent with WebMercatorExtents).

--- src/main/java/com/conveyal/r5/analyst/WebMercatorGridPointSet.java
+++ src/main/java/com/conveyal/r5/analyst/WebMercatorGridPointSet.java
@@ -27,46 +27,17 @@ public class WebMercatorGridPointSet extends PointSet implements Serializable {
 
     public static final Logger LOG = LoggerFactory.getLogger(WebMercatorGridPointSet.class);
 
-    /**
-     * Default Web Mercator zoom level for grids (origin/destination layers, aggregation area masks, etc.).
-     * Level 10 is probably ideal but will quadruple calculation relative to 9.
-     */
-    public static final int DEFAULT_ZOOM = 9;
-
-    public static final int MIN_ZOOM = 9;
-
-    public static final int MAX_ZOOM = 12;
-
-    /** web mercator zoom level */
-    public final int zoom;
-
-    /** westernmost pixel  */
-    public final int west;
-
-    /** northernmost pixel */
-    public final int north;
-
-    /** The number of pixels across this grid in the east-west direction. */
-    public final int width;
-
-    /** The number of pixels from top to bottom of this grid in the north-south direction. */
-    public final int height;
-
+    public final WebMercatorExtents extents;
     /** Base pointset; linkages will be shared with this pointset */
     public final WebMercatorGridPointSet basePointSet;
 
     /**
      * Create a new WebMercatorGridPointSet.
-     *
      * @oaram basePointSet the super-grid pointset from which linkages will be copied or shared, or null if no
      *        such grid exists.
      */
     public WebMercatorGridPointSet(int zoom, int west, int north, int width, int height, WebMercatorGridPointSet basePointSet) {
-        this.zoom = zoom;
-        this.west = west;
-        this.north = north;
-        this.width = width;
-        this.height = height;
+        this.extents = new WebMercatorExtents(west, north, width, height, zoom);
         this.basePointSet = basePointSet;
     }
 
@@ -85,15 +56,8 @@ public class WebMercatorGridPointSet extends PointSet implements Serializable {
     public WebMercatorGridPointSet (Envelope wgsEnvelope) {
         LOG.info("Creating WebMercatorGridPointSet with WGS84 extents {}", wgsEnvelope);
         checkWgsEnvelopeSize(wgsEnvelope, "grid point set");
-        this.zoom = DEFAULT_ZOOM;
-        int west = lonToPixel(wgsEnvelope.getMinX(), zoom);
-        int east = lonToPixel(wgsEnvelope.getMaxX(), zoom);
-        int north = latToPixel(wgsEnvelope.getMaxY(), zoom);
-        int south = latToPixel(wgsEnvelope.getMinY(), zoom);
-        this.west = west;
-        this.north = north;
-        this.height = south - north;
-        this.width = east - west;
+        // NOTE the width and height calculations were wrong before, use standard ones in WebMercatorExtents
+        this.extents = WebMercatorExtents.forWgsEnvelope(wgsEnvelope, WebMercatorExtents.DEFAULT_ZOOM); // USE TRIMMED?
         this.basePointSet = null;
     }
 
@@ -104,7 +68,7 @@ public class WebMercatorGridPointSet extends PointSet implements Serializable {
 
     @Override
     public int featureCount() {
-        return height * width;
+        return extents.height * extents.width;
     }
 
     @Override
@@ -115,38 +79,40 @@ public class WebMercatorGridPointSet extends PointSet implements Serializable {
 
     @Override
     public double getLat(int i) {
-        long y = i / this.width + this.north;
-        return pixelToLat(y, zoom);
+        long y = i / extents.width + extents.north;
+        return pixelToLat(y, extents.zoom);
     }
 
     @Override
     public double getLon(int i) {
-        long x = i % this.width + this.west;
-        return pixelToLon(x, zoom);
+        long x = i % extents.width + extents.west;
+        return pixelToLon(x, extents.zoom);
     }
 
     @Override
     public TIntList getPointsInEnvelope (Envelope envelopeFixedDegrees) {
         // Convert fixed-degree envelope to floating, then to world-scale web Mercator pixels at this grid's zoom level.
         // This is not very DRY since we do something very similar in the constructor and elsewhere.
-        int west = lonToPixel(fixedDegreesToFloating(envelopeFixedDegrees.getMinX()), zoom);
-        int east = lonToPixel(fixedDegreesToFloating(envelopeFixedDegrees.getMaxX()), zoom);
-        int north = latToPixel(fixedDegreesToFloating(envelopeFixedDegrees.getMaxY()), zoom);
-        int south = latToPixel(fixedDegreesToFloating(envelopeFixedDegrees.getMinY()), zoom);
+        // These are the integer pixel numbers containing the envelope, so iteration below must be inclusive of the max.
+        final int z = extents.zoom;
+        int west = lonToPixel(fixedDegreesToFloating(envelopeFixedDegrees.getMinX()), z);
+        int east = lonToPixel(fixedDegreesToFloating(envelopeFixedDegrees.getMaxX()), z);
+        int north = latToPixel(fixedDegreesToFloating(envelopeFixedDegrees.getMaxY()), z);
+        int south = latToPixel(fixedDegreesToFloating(envelopeFixedDegrees.getMinY()), z);
         // Make the envelope's pixel values relative to the edges of this WebMercatorGridPointSet, rather than
         // absolute world-scale coordinates at this zoom level.
-        west -= this.west;
-        east -= this.west;
-        north -= this.north;
-        south -= this.north;
+        west -= extents.west;
+        east -= extents.west;
+        north -= extents.north;
+        south -= extents.north;
         TIntList pointsInEnvelope = new TIntArrayList();
         // Pixels are truncated toward zero, and coords increase toward East and South in web Mercator, so <= south/east.
         for (int y = north; y <= south; y++) {
-            if (y < 0 || y >= this.height) continue;
+            if (y < 0 || y >= extents.height) continue;
             for (int x = west; x <= east; x++) {
-                if (x < 0 || x >= this.width) continue;
+                if (x < 0 || x >= extents.width) continue;
                 // Calculate the 1D (flattened) index into this pointset for the grid cell at (x,y).
-                int pointIndex = y * this.width + x;
+                int pointIndex = y * extents.width + x;
                 pointsInEnvelope.add(pointIndex);
             }
         }
@@ -162,8 +128,8 @@ public class WebMercatorGridPointSet extends PointSet implements Serializable {
     //@Override
     // TODO add this to the PointSet interface
     public String getPointId (int i) {
-        int y = i / this.width;
-        int x = i % this.width;
+        int y = i / extents.width;
+        int x = i % extents.width;
         return x + "," + y;
     }
 
@@ -172,14 +138,17 @@ public class WebMercatorGridPointSet extends PointSet implements Serializable {
     }
 
     public int getPointIndexContaining (double lon, double lat) {
-        int x = lonToPixel(lon, zoom) - west;
-        int y = latToPixel(lat, zoom) - north;
-        return y * width + x;
+        int x = lonToPixel(lon, extents.zoom) - extents.west;
+        int y = latToPixel(lat, extents.zoom) - extents.north;
+        return y * extents.width + x;
     }
 
     @Override
-    public String toString () {
-        return "WebMercatorGridPointSet{" + "zoom=" + zoom + ", west=" + west + ", north=" + north + ", width=" + width + ", height=" + height + ", basePointSet=" + basePointSet + '}';
+    public String toString() {
+        return "WebMercatorGridPointSet{" +
+                "extents=" + extents +
+                ", basePointSet=" + basePointSet +
+                '}';
     }
 
     @Override
@@ -189,13 +158,8 @@ public class WebMercatorGridPointSet extends PointSet implements Serializable {
 
     @Override
     public WebMercatorExtents getWebMercatorExtents () {
-        return new WebMercatorExtents(west, north, width, height, zoom);
-    }
-
-    public static int parseZoom(String zoomString) {
-        int zoom = (zoomString == null) ? DEFAULT_ZOOM : Integer.parseInt(zoomString);
-        checkArgument(zoom >= MIN_ZOOM && zoom <= MAX_ZOOM);
-        return zoom;
+        // WebMercatorExtents are immutable so we can return this without making a protective copy.
+        return extents;
     }
 
 }

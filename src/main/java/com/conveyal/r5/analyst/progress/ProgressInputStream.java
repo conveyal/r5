6bceedc48df11d51989b5579e00bcb0b0fb897e4
Trevor Gerhardt <trevorgerhardt@gmail.com>
2023-10-27 19:28:44 +0800

    Remove unused method

--- src/main/java/com/conveyal/r5/analyst/progress/ProgressInputStream.java
+++ src/main/java/com/conveyal/r5/analyst/progress/ProgressInputStream.java
@@ -1,7 +1,6 @@
 package com.conveyal.r5.analyst.progress;
 
 import com.conveyal.file.FileUtils;
-import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.io.input.ProxyInputStream;
 
 import java.io.File;
@@ -15,7 +14,7 @@ import static org.apache.commons.io.IOUtils.EOF;
  * This will report progress as the total number of bytes that have passed through the stream, like CountingInputStream.
  * This can exceed 100% of the file size if the caller uses mark and reset. The progressListener should be
  * pre-configured with the total number of bytes expected and a detail message using ProgressListener::beginTask.
- * The static method forFileItem() demonstrates usage when reading from a file of known length.
+ * The static method forFile() demonstrates usage when reading from a file of known length.
  */
 public class ProgressInputStream extends ProxyInputStream {
 
@@ -38,22 +37,6 @@ public class ProgressInputStream extends ProxyInputStream {
         final long skippedBytes = super.skip(length);
         progressListener.increment((int) skippedBytes);
         return skippedBytes;
-    }
-
-    /**
-     * Given an uploaded file, report progress on reading it.
-     * Incrementing the progress seems to introduce some inefficiency when performing unbuffered small reads, such as
-     * calls to InputStream.read() which are used by DataInputStream to read numbers.
-     * TODO wrap in buffered input stream to reduce small read calls, or tune to only report once per percentage?
-     */
-    public static ProgressInputStream forFileItem (FileItem fileItem, ProgressListener progressListener) {
-        try {
-            checkArgument(fileItem.getSize() < Integer.MAX_VALUE);
-            progressListener.beginTask("Reading file " + fileItem.getName(), (int)(fileItem.getSize()));
-            return new ProgressInputStream(progressListener, fileItem.getInputStream());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     /**

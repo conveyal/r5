8050f71f0c9148452a9d3a2689f4b6adcbce26cb
Trevor Gerhardt <trevorgerhardt@gmail.com>
2023-11-03 11:36:30 +0800

    Add missing OD check back into the creation process

--- src/main/java/com/conveyal/analysis/controllers/RegionalAnalysisController.java
+++ src/main/java/com/conveyal/analysis/controllers/RegionalAnalysisController.java
@@ -565,6 +565,7 @@ public class RegionalAnalysisController implements HttpController {
             task.destinationPointSets = new PointSet[] {
                     PointSetCache.readFreeFormFromFileStore(task.destinationPointSetKeys[0])
             };
+            MultiOriginAssembler.ensureOdPairsUnderLimit(task, task.destinationPointSets[0]);
         }
         if (task.recordTimes) {
             checkArgument(

4a62f1653ad93754ce4facd38e26bd9210b991a8
ansons <ansonstewart@gmail.com>
2025-09-09 16:31:05 -0400

    Correct javadoc

--- src/main/java/com/conveyal/r5/labeling/NoSidewalkCyclingTraversalPermissionLabeler.java
+++ src/main/java/com/conveyal/r5/labeling/NoSidewalkCyclingTraversalPermissionLabeler.java
@@ -4,13 +4,7 @@ import com.conveyal.osmlib.Way;
 import com.conveyal.r5.streets.EdgeStore;
 
 /**
- * Assign traversal permissions to edges based on their tags in OpenStreetMap.
- * see https://wiki.openstreetmap.org/wiki/Computing_access_restrictions#Algorithm
- * and also prior work by Marko Burjek:
- * https://github.com/buma/OpenTripPlanner-Maribor/blob/8eafa3ad9f1426877c6da3d730eaea46c6de35cf/src/main/java/org/opentripplanner/streets/permissions/AccessRestrictionsAlgorithm.java
- *
- * This class is abstract. You must make a country-specific subclass containing defaults for a particular country,
- * see https://wiki.openstreetmap.org/wiki/OSM_tags_for_routing/Access-Restrictions
+ * USTraversalPermissionLabeler, except biking is disallowed on ways with footway=sidewalk tag
  */
 public class NoSidewalkCyclingTraversalPermissionLabeler extends USTraversalPermissionLabeler {
     @Override

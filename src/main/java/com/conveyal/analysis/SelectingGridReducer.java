ba3da7e7e0e331bcb8963a6b9e86fc41d7786775
Andrew Byrd <andrew@fastmail.net>
2025-12-04 14:42:07 +0800

    use ByteBuffer.order() to reverse int bytes
    instead of LittleEndianInputStream
    for 32k origins, takes 30 msec per cutoff extracted

--- src/main/java/com/conveyal/analysis/SelectingGridReducer.java
+++ src/main/java/com/conveyal/analysis/SelectingGridReducer.java
@@ -1,10 +1,12 @@
 package com.conveyal.analysis;
 
 import com.conveyal.r5.analyst.Grid;
-import com.google.common.io.LittleEndianDataInputStream;
 
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.zip.GZIPInputStream;
 
 /**
@@ -26,60 +28,67 @@ public class SelectingGridReducer {
     /** Version of the access grid format we read */
     private static final int ACCESS_GRID_VERSION = 0;
 
-    public final int index;
+    private final int index;
+
+    private final ByteBuffer swapBuffer = ByteBuffer.allocate(8);
 
     /** Initialize with the index to extract */
     public SelectingGridReducer(int index) {
         this.index = index;
     }
 
-    public Grid compute (InputStream rawInput) throws IOException {
-        LittleEndianDataInputStream input = new LittleEndianDataInputStream(new GZIPInputStream(rawInput));
-
-        char[] header = new char[8];
-        for (int i = 0; i < 8; i++) {
-            header[i] = (char) input.readByte();
-        }
-
-        if (!"ACCESSGR".equals(new String(header))) {
-            throw new IllegalArgumentException("Input not in access grid format!");
-        }
-
-        int version = input.readInt();
-
-        // FIXME access grid reading logic should not be embedded in this reduce operation
-        if (version != ACCESS_GRID_VERSION) {
-            throw new IllegalArgumentException(String.format("Version mismatch of access grids, expected %s, found %s", ACCESS_GRID_VERSION, version));
-        }
-
-        int zoom = input.readInt();
-        int west = input.readInt();
-        int north = input.readInt();
-        int width = input.readInt();
-        int height = input.readInt();
-
-        // The number of samples stored at each origin; these could be instantaneous accessibility values for each
-        // Monte Carlo draw, or they could be bootstrap replications of a sampling distribution of accessibility given
-        // median travel time.
-        int nSamples = input.readInt();
-
-        Grid outputGrid = new Grid(west, north, width, height, zoom);
-
-        int[] valuesThisOrigin = new int[nSamples];
-
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                // input values are delta-coded per origin, so use val to keep track of current value
-                for (int iteration = 0, val = 0; iteration < nSamples; iteration++) {
-                    val += input.readInt();
-                    valuesThisOrigin[iteration] = val;
-                }
-                // compute percentiles
-                outputGrid.grid[x][y] = valuesThisOrigin[index];
+    public Grid compute (InputStream rawInput) {
+        try (DataInputStream input = new DataInputStream(new GZIPInputStream(rawInput))) {
+            // Ideally, this access grid reading logic should not be embedded in this reduce operation.
+            char[] header = new char[8];
+            for (int i = 0; i < 8; i++) {
+                header[i] = (char) input.readByte();
             }
+            if (!"ACCESSGR".equals(new String(header))) {
+                throw new IllegalArgumentException("Input not in access grid format!");
+            }
+            int version = readIntSwap(input);
+            if (version != ACCESS_GRID_VERSION) {
+                throw new IllegalArgumentException(String.format("Version mismatch of access grids, expected %s, found %s", ACCESS_GRID_VERSION, version));
+            }
+            int zoom = readIntSwap(input);
+            int west = readIntSwap(input);
+            int north = readIntSwap(input);
+            int width = readIntSwap(input);
+            int height = readIntSwap(input);
+
+            // The number of samples stored at each origin; these could be instantaneous accessibility values for each
+            // Monte Carlo draw, or they could be bootstrap replications of a sampling distribution of accessibility given
+            // median travel time.
+            int nSamples = readIntSwap(input);
+            Grid outputGrid = new Grid(west, north, width, height, zoom);
+            int[] valuesThisOrigin = new int[nSamples];
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    // input values are delta-coded per origin, so use val to keep track of current value
+                    for (int iteration = 0, val = 0; iteration < nSamples; iteration++) {
+                        val += readIntSwap(input);
+                        valuesThisOrigin[iteration] = val;
+                    }
+                    // compute percentiles
+                    outputGrid.grid[x][y] = valuesThisOrigin[index];
+                }
+            }
+            input.close();
+            return outputGrid;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        input.close();
-        return outputGrid;
     }
 
+    /// NOT THREAD SAFE, reuses per-instance byte buffer
+    private int swap (int x) {
+        swapBuffer.order(ByteOrder.nativeOrder()).putInt(0, x);
+        return swapBuffer.order(ByteOrder.BIG_ENDIAN).getInt(0);
+    }
+
+    private int readIntSwap (DataInputStream dis) throws IOException {
+        int swapped = dis.readInt();
+        return swap(swapped);
+    }
 }

ec3d72002bcb612524fe312a4b33564b8e93dbf7
Anson Stewart <astewart@conveyal.com>
2026-05-27 18:38:00 -0400

    Add 3 sec penalty to traverse any intersection
    This may have unintended effects, especially along paths with many vertices (e.g., minor driveway intersections)

--- src/main/java/com/conveyal/r5/streets/BasicTraversalTimeCalculator.java
+++ src/main/java/com/conveyal/r5/streets/BasicTraversalTimeCalculator.java
@@ -21,7 +21,7 @@ public class BasicTraversalTimeCalculator implements TraversalTimeCalculator {
     // TODO re-express as "turn against traffic" and "turn with traffic" to make this less confusing.
 
     public static final int LEFT_TURN = 30;
-    public static final int STRAIGHT_ON = 0;
+    public static final int STRAIGHT_ON = 3;
     public static final int RIGHT_TURN = 10;
     public static final int U_TURN = 90; // penalize U turns extremely heavily
 
